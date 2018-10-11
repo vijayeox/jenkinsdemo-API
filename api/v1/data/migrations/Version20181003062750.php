3c220e4d613dcc241d0870d61f3c26cec7b61f04
bharat <bharatg@myvamla.com>
2018-10-11 16:25:50 -0400

    Add Migration Scripts for bookmarks

--- api/v1/data/migrations/Version20181003062750.php
+++ api/v1/data/migrations/Version20181003062750.php
@@ -14,6 +14,8 @@ final class Version20181003062750 extends AbstractMigration
     {
         $this->addSql("ALTER TABLE `links` CHANGE `orgid` `org_id` INT(11) NOT NULL;");
         $this->addSql("ALTER TABLE `links` CHANGE `avatarid` `avatar_id` INT(11) NULL DEFAULT NULL;");
+        $this->addSql("INSERT INTO ox_privilege (name,permission_allowed) values ('MANAGE_BOOKMARK',15);");
+        $this->addSql("INSERT INTO ox_role_privilege (role_id,privilege_name,permission) values (1, 'MANAGE_BOOKMARK',15);");
     }
 
     public function down(Schema $schema) : void
