41b94cf70f9c66426a7ad58720a99aa26a8160ee
bharat <bharatg@myvamla.com>
2018-11-14 15:55:10 +0530

    Bug Fixes in Migrations

--- api/v1/data/migrations/Version20181105095934.php
+++ api/v1/data/migrations/Version20181105095934.php
@@ -10,47 +10,47 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20181105095934 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
-    {
+	public function up(Schema $schema) : void
+	{
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("CREATE TABLE IF NOT EXISTS `ox_group` (
-			  `id` int(11) NOT NULL AUTO_INCREMENT,
-			  `name` varchar(20000) CHARACTER SET utf8 NOT NULL,
-			  `parent_id` int(11) NOT NULL,
-			  `org_id` int(11) NOT NULL,
-			  `manager_id` int(11) NOT NULL,
-			  `description` mediumtext,
-			  `logo` varchar(20) DEFAULT NULL,
-			  `cover_photo` varchar(111) DEFAULT NULL,
-			  `type` tinyint(4) NOT NULL DEFAULT '0',
-			  `status` enum('Active','Inactive') NOT NULL DEFAULT 'Active',
-			  `date_created` datetime NOT NULL,
-			  `date_modified` datetime NOT NULL,
-			  `created_id` int(11) NOT NULL,
-			  `modified_id` int(11) NOT NULL,
-			  PRIMARY KEY (`id`),
-			  KEY `orgid` (`orgid`),
-			  KEY `status` (`status`)
-			) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1");
+		$this->addSql("CREATE TABLE IF NOT EXISTS `ox_group` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`name` varchar(20000) CHARACTER SET utf8 NOT NULL,
+			`parent_id` int(11) NOT NULL,
+			`org_id` int(11) NOT NULL,
+			`manager_id` int(11) NOT NULL,
+			`description` mediumtext,
+			`logo` varchar(20) DEFAULT NULL,
+			`cover_photo` varchar(111) DEFAULT NULL,
+			`type` tinyint(4) NOT NULL DEFAULT '0',
+			`status` enum('Active','Inactive') NOT NULL DEFAULT 'Active',
+			`date_created` datetime NOT NULL,
+			`date_modified` datetime NOT NULL,
+			`created_id` int(11) NOT NULL,
+			`modified_id` int(11) NOT NULL,
+			PRIMARY KEY (`id`),
+			KEY `orgid` (`org_id`),
+			KEY `status` (`status`)
+		) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1");
 
-
-
-        $this->addSql("CREATE TABLE IF NOT EXISTS `ox_user_group` (
-			  `id` int(11) NOT NULL AUTO_INCREMENT,
-			  `group_id` int(11) NOT NULL,
-			  `avatar_id` int(11) NOT NULL,
-			  PRIMARY KEY (`id`),
-			  KEY `avatarid` (`avatar_id`)
+		$this->addSql("CREATE TABLE IF NOT EXISTS `ox_user_group` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`group_id` int(11) NOT NULL,
+			`avatar_id` int(11) NOT NULL,
+			PRIMARY KEY (`id`),
+			KEY `avatarid` (`avatar_id`)
 			) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
-			SELECT * FROM rakshithapi.ox_user_group;");
+			SELECT * FROM ox_user_group;");
+		$this->addSql("INSERT INTO ox_privilege (name,permission_allowed) values ('MANAGE_GROUP',15);");
+		$this->addSql("INSERT INTO ox_role_privilege (role_id,privilege_name,permission) values (1, 'MANAGE_GROUP',15);");
 
-    }
+	}
 
-    public function down(Schema $schema) : void
-    {
+	public function down(Schema $schema) : void
+	{
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql("DROP TABLE ox_group");
-        $this->addSql("DROP TABLE ox_user_group");
+		$this->addSql("DROP TABLE ox_group");
+		$this->addSql("DROP TABLE ox_user_group");
 
-    }
+	}
 }
