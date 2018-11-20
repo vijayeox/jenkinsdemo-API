9e7d1547cf79e051a3a84c92c525b69a72245d10
Rajesh S <rajesha.s@gmail.com>
2018-11-20 13:54:24 +0530

    Camunda docker setup and a spring boot app for starting camel. TODO - dockerizing the camel

---
+++ integrations/src/main/groovy/com/oxzion/Application.groovy
@@ -0,0 +1,10 @@
+package com.oxzion
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication
+
+@SpringBootApplication
+class Application{
+    public static void main(String[] args){
+        SpringApplication.run Application, args
+    }
+}
