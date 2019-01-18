a60feda7f839921e1009f831b8c1d00de57d829c
bharat <bharatg@myvamla.com>
2019-01-18 10:41:24 +0530

    Add XML Utils

---
+++ api/v1/lib/Oxzion/src/Utils/XMLUtils.php
@@ -0,0 +1,46 @@
+<?php
+namespace Oxzion\Utils;
+
+class XMLUtils {
+
+	static public function domToArray($root) {
+		$result = array();
+		if ($root->hasAttributes()) {
+			$attrs = $root->attributes;
+			foreach ($attrs as $i => $attr) {
+				$result[$attr->name] = $attr->value;
+			}
+		}
+		$children = $root->childNodes;
+		if($children){
+			if ($children->length == 1) {
+				$child = $children->item(0);
+				if ($child->nodeType == XML_TEXT_NODE) {
+					$result['_value'] = $child->nodeValue;
+					if (count($result) == 1) {
+						return $result['_value'];
+					} else {
+						return $result;
+					}
+				}
+			}
+		}
+		$group = array();
+		if(isset($children->length)){
+			for($i = 0; $i < $children->length; $i++) {
+				$child = $children->item($i);
+				if (!isset($result[$child->nodeName])) {
+					$result[$child->nodeName] = self::domToArray($child);
+				} else {
+					if (!isset($group[$child->nodeName])) {
+						$tmp = $result[$child->nodeName];
+						$result[$child->nodeName] = array($tmp);
+						$group[$child->nodeName] = 1;
+					} 
+					$result[$child->nodeName][] = self::domToArray($child);
+				}
+			}
+		}
+		return $result;
+	} 
+}
