3014b010be8b03e627cb148c6ebe51879a2930f6
bharat <bharatg@myvamla.com>
2019-02-02 14:38:49 +0530

    Add Cors Allow from origin

--- api/v1/public/index.php
+++ api/v1/public/index.php
@@ -21,6 +21,13 @@ if (php_sapi_name() === 'cli-server') {
     }
     unset($path);
 }
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
