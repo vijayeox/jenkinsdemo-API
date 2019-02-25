449710f8f59eb09a1c9038e152194e54a0dba30a
bharatg <bharatg@myvamla.com>
2019-02-26 03:22:45 +0530

    Update Props for Web Sockets

--- integrations/camel/src/main/groovy/com/oxzion/routes/WebSocket.groovy
+++ integrations/camel/src/main/groovy/com/oxzion/routes/WebSocket.groovy
@@ -5,6 +5,7 @@ import org.apache.camel.CamelContext
 import org.apache.camel.Exchange
 import org.apache.camel.Processor
 import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.component.properties.PropertiesComponent
 import org.apache.camel.impl.DefaultCamelContext
 import org.springframework.stereotype.Component
 
@@ -16,12 +17,14 @@ class WebSocket extends RouteBuilder {
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() {
+                PropertiesComponent propc = getContext().getComponent("properties", PropertiesComponent.class)
+                propc.setLocation("classpath:oxzion.properties")
                 from("activemq:topic:notification").process(new Processor() {
                     public void process(Exchange exchange) throws Exception {
                         def jsonSlurper = new JsonSlurper()
                         def object = jsonSlurper.parseText(exchange.getMessage().getBody() as String)
                     }
-                }).to("log:notification").to("ahc-ws://{websocket.host}:{websocket.port}/{websocket.channel}")
+                }).to("log:notification").to("ahc-ws://{{websocket.host}}:{{websocket.port}}/{{websocket.channel}}")
 
             }
         })
