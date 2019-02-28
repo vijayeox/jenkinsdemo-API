254ef0632b310694ed69e19d7d29bac33637cee5
Sadhitha <saditha@myvamla.com>
2019-02-28 16:59:57 +0530

    Removed Print statements

--- api/v1/lib/Oxzion/src/Utils/RestClient.php
+++ api/v1/lib/Oxzion/src/Utils/RestClient.php
@@ -6,14 +6,30 @@ use GuzzleHttp\Psr7\MultipartStream;
 class RestClient{
   private $client;
   public function __construct($baseUrl){
-    $this->client = new Client(['base_uri' => $baseUrl,'timeout'  => 2.0,]);
+    $this->client = new Client(['base_uri' => $baseUrl,'timeout'  => 10.0,]);
   }
-  public function get($url,$params=array()){
-    $response = $this->client->request('GET',$baseUrl.$url, $params);
+  public function get($url,$params=array(),$headers=null){
+    $payload = array();
+    if(isset($params) && !empty($params)){
+      $payload['json'] = $params;
+    }
+    if(isset($headers) && !empty($headers)){
+        $payload['headers'] = $headers;
+    }
+
+    $response = $this->client->request('GET',$baseUrl.$url, $payload);
+    
     return $response->getBody()->getContents();
   }
-  public function delete($url,$params=array()){
-    $response = $this->client->request('DELETE',$baseUrl.$url,$params);
+  public function delete($url,$params=array(),$headers=null){
+    $payload = array();
+    if(isset($params) && !empty($params)){
+      $payload['json'] = $params;
+    }
+    if(isset($headers) && !empty($headers)){
+        $payload['headers'] = $headers;
+    }
+    $response = $this->client->request('DELETE',$baseUrl.$url,$payload);
     return $response->getBody()->getContents();
   }
   public function postMultiPart($url,$formParams=array(),$fileParams=array()){
@@ -45,8 +61,23 @@ class RestClient{
       return 0;
     }
   }
-  public function put($url,$params){
-    $response = $client->request('PUT',$baseUrl.$url);
+  public function postWithHeader($url,$formParams=array(),$headers=array()){
+    try {
+      $response = $this->client->request('POST', $url,['headers' => $headers,'json' => $formParams]);
+        return array('body'=>$response->getBody()->getContents(),'headers'=>$response->getHeaders());
+    } catch(Exception $e){
+        return 0;
+    }
+  }
+  public function put($url,$params=array(),$headers=null){
+    $payload = array();
+    if(isset($params) && !empty($params)){
+      $payload['json'] = $params;
+    }
+    if(isset($headers) && !empty($headers)){
+        $payload['headers'] = $headers;
+    }
+    $response = $this->client->request('PUT',$baseUrl.$url,$payload);
     return $response->getBody()->getContents();
   }
 }
