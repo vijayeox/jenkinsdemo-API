cc031757253a5238eee02ddb84599ffba88b9516
bharatg <bharatg@myvamla.com>
2019-03-13 22:07:37 +0530

    Fix privilege controller tests

--- api/v1/module/Privilege/src/Service/PrivilegeService.php
+++ api/v1/module/Privilege/src/Service/PrivilegeService.php
@@ -23,10 +23,10 @@ class PrivilegeService extends AbstractService
         try {
             $userId = AuthContext::get(AuthConstants::USER_ID);
             $queryString = "select op.name, op.permission_allowed from ox_privilege as op
- LEFT JOIN ox_role_privilege as orp on orp.app_id = op.app_id
- LEFT JOIN ox_role as orl on orl.id = orp.role_id
- LEFT JOIN ox_user_role as our on our.role_id = orl.id
- LEFT JOIN  ox_user as av on av.id = our.user_id ";
+                            LEFT JOIN ox_role_privilege as orp on orp.app_id = op.app_id
+                            LEFT JOIN ox_role as orl on orl.id = orp.role_id
+                            LEFT JOIN ox_user_role as our on our.role_id = orl.id
+                            LEFT JOIN  ox_user as av on av.id = our.user_id ";
             $where = "where av.id = " . $userId . " and op.app_id = '" . $appId . "'";
             $order = "order by av.firstname";
             $group = "group by op.name";
