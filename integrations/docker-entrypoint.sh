2a1b07ef58b1f45cd3c2f9c41d4cbed5d267695b
danishsultan <danishsultan007@gmail.com>
2019-03-14 20:29:41 +0530

    docker-compose for chat and php-apps

--- integrations/docker-entrypoint.sh
+++ integrations/docker-entrypoint.sh
@@ -10,12 +10,11 @@ mkdir /var/www/public/rainloop/rainloop
 cp -R build/dist/releases/webmail/1.12.1/src/rainloop /var/www/public/rainloop/
 cp build/dist/releases/webmail/1.12.1/src/data/* /var/www/public/rainloop/data
 
-cp -R /integrations/eventcalendar /var/www/public/calendar/    
+cp -R /integrations/eventcalendar/* /var/www/public/calendar   
+chown www-data:www-data -R /var/www/public
 
-cd /integrations/orocrm
-composer install
-cp -R /integrations/orocrm /var/www/public/crm/
+#cd /integrations/orocrm
+#composer install
+#cp -R /integrations/orocrm /var/www/public/crm/
 service apache2 start
-tail -f /dev/null &
-
-    `
+tail -f /dev/null
