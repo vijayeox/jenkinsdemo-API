cbf919c34df5c89d080b271d9cfe199c84b4c1ae
bharatg <bharatg@myvamla.com>
2019-03-20 20:24:48 +0530

    PHP Docker file Add plugins and CRM Build

--- integrations/docker-entrypoint.sh
+++ integrations/docker-entrypoint.sh
@@ -13,8 +13,13 @@ cp build/dist/releases/webmail/1.12.1/src/data/* /var/www/public/rainloop/data
 cp -R /integrations/eventcalendar/* /var/www/public/calendar   
 chown www-data:www-data -R /var/www/public
 
-#cd /integrations/orocrm
-#composer install
-#cp -R /integrations/orocrm /var/www/public/crm/
+cp -ra /integrations/orocrm/. /var/www/public/crm/
+cd /var/www/public/crm/
+chmod 777 -R /var/www/public/crm/
+composer install
+php ./bin/console oro:install --env=prod --timeout=30000 --application-url="http://localhost:8075/crm/public" --organization-name="Vantage Agora" --user-name="admin" --user-email="admin@example.com" --user-firstname="Admin" --user-lastname="User" --user-password="admin" --language=en --formatting-code=en_US
+cp ./orocrm_supervisor.conf /etc/supervisor/conf.d/
+
 service apache2 start
-tail -f /dev/null
+service supervisor restart
+tail -f /dev/null &
