349c7b155bbd9b2c54820b44fced58b165d7ac60
bharatg <bharatg@myvamla.com>
2019-03-22 00:50:18 +0530

    CRM Missing Bundles

--- integrations/docker-entrypoint.sh
+++ integrations/docker-entrypoint.sh
@@ -19,6 +19,10 @@ chmod 777 -R /var/www/public/crm/
 composer install
 php ./bin/console oro:install --env=prod --timeout=30000 --application-url="http://localhost:8075/crm/public" --organization-name="Vantage Agora" --user-name="admin" --user-email="admin@example.com" --user-firstname="Admin" --user-lastname="User" --user-password="admin" --language=en --formatting-code=en_US
 cp ./orocrm_supervisor.conf /etc/supervisor/conf.d/
+# Relative URL Issue
+mkdir -p /var/www/public/crm/public/css/themes/oro/bundles/bowerassets/font-awesome
+cp -R /var/www/public/crm/public/bundles/bowerassets/font-awesome/ /var/www/public/crm/public/css/themes/oro/bundles/bowerassets/font-awesome
+chmod 777 -R /var/www/
 
 service apache2 start
 service supervisor restart
