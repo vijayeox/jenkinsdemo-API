d370f01359a4e7c50b209645321b1ce187335440
meyer auslander <meyer@myvamla.com>
2019-05-21 16:17:28 -0400

    Splash Page Migration Script

---
+++ api/v1/data/migrations/Version20190521201540.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190521172350 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $sql = "CREATE TABLE `ox_splashpage` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `org_id` int(11) DEFAULT NULL,
+            `content` text,
+            `enabled` int(11) NOT NULL DEFAULT '1',
+            PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1 COMMENT='splash page content\n\n';
+        ";
+        $this->addSql($sql);
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("DROP TABLE ox_splashpage");
+    }
+}
+
