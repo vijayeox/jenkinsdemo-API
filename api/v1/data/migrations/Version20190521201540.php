9967149e79428493c7953f500581230bba8cae42
bharatg <bharatg@myvamla.com>
2019-05-23 16:32:25 +0530

    Fix Issue in Migration

--- api/v1/data/migrations/Version20190521201540.php
+++ api/v1/data/migrations/Version20190521201540.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190521172350 extends AbstractMigration
+final class Version20190521201540 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
