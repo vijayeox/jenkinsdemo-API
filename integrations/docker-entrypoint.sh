f7b7a6ca4d513bc9e407d95dc7d36c07f46cfcbd
bharatg <bharatg@myvamla.com>
2019-06-08 11:49:28 +0530

    Calendar Bug Fixes for deployment

--- integrations/docker-entrypoint.sh
+++ integrations/docker-entrypoint.sh
@@ -6,16 +6,17 @@ npm audit fix
 gulp rainloop:start
 
 cp build/dist/releases/webmail/1.12.1/src/*.php /var/www/public/rainloop
-mkdir /var/www/public/rainloop/rainloop
+mkdir -p /var/www/public/rainloop/rainloop
 cp -R build/dist/releases/webmail/1.12.1/src/rainloop /var/www/public/rainloop/
 cp build/dist/releases/webmail/1.12.1/src/data/* /var/www/public/rainloop/data
 
-cp -R /integrations/eventcalendar/* /var/www/public/calendar   
 chown www-data:www-data -R /var/www/public
 
-cp -ra /integrations/orocrm/. /var/www/public/crm/
-cd /var/www/public/crm/
-chmod 777 -R /var/www/public/crm/
+ln -s /integrations/eventcalendar/* /var/www/public/calendar   
+
+ln -s /integrations/orocrm/* /var/www/public/crm
+cd /integrations/orocrm/
+chmod 777 -R .
 composer install
 php ./bin/console oro:install --env=prod --timeout=30000 --application-url="http://localhost:8075/crm/public" --organization-name="Vantage Agora" --user-name="admin" --user-email="admin@example.com" --user-firstname="Admin" --user-lastname="User" --user-password="admin" --language=en --formatting-code=en_US
 cp ./orocrm_supervisor.conf /etc/supervisor/conf.d/
