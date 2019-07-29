5a4917754a69729286b8a5ad5bb5e499a2173eb0
Sadhitha <saditha@myvamla.com>
2019-07-29 14:28:48 +0530

    Repository Setup for HUB

---
+++ clients/hub/view/apps/Hub/index.js
@@ -0,0 +1,28 @@
+import './index.scss';
+import osjs from 'osjs';
+import React from "react";
+import ReactDOM from "react-dom";
+import menu from './menu.json';
+import {name as applicationName} from './metadata.json';
+import RenderTemplate from "../../../../../view/gui/src/rendertemplate.js";
+
+// Our launcher
+const register = (core, args, options, metadata) => {
+  // Create a new Application instance
+  const proc = core.make('osjs/application', {args, options, metadata});
+ 
+  // Create  a new Window instance
+  proc.createWindow({
+    id: 'HubWindow',
+    title: metadata.title.en_EN,
+    dimension: {width: 400, height: 400},
+    position: {left: 700, top: 200}
+  })
+    .on('destroy', () => proc.destroy())
+    .render($content => ReactDOM.render(<RenderTemplate args={core} menu={menu}/>, $content));
+
+   return proc;
+};
+
+// Creates the internal callback function when OS.js launches an application
+osjs.register(applicationName, register);
