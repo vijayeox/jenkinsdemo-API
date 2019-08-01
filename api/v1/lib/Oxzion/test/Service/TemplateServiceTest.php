a4f20c924efb41fe57a6218ce19661ec450d5032
Rajesh S <rajesha.s@gmail.com>
2019-08-01 16:33:42 +0530

    Added missing assertion

--- api/v1/lib/Oxzion/test/Service/TemplateServiceTest.php
+++ api/v1/lib/Oxzion/test/Service/TemplateServiceTest.php
@@ -41,7 +41,8 @@ class TemplateServiceTest extends ServiceTest {
         copy(__DIR__."/template/GenericTemplate.tpl", $tempFile."GenericTemplate.tpl"); 
         $TemplateService = new TemplateService($config, $this->adapter);
         $content = $TemplateService->getContent('GenericTemplate',$data);
-        // $this->assertEquals("<p>Hello ".$data['username'].", this is a generic template.</p>", $content);
+        $temp = "Hello ".$data['username'].", this is a generic template.</p>";
+        $this->assertEquals(strpos($content, $temp), 3);
         $templateName="GenericTemplate.tpl";
         FileUtils::deleteFile($templateName,$tempFile);
       
