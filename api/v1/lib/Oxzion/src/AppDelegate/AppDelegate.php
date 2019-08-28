ec00229f983e4b5aa089a9cb9a2c4c57de8096ab
Rajesh S <rajesha.s@gmail.com>
2019-08-28 16:59:10 +0530

    Made coe type safe

--- api/v1/lib/Oxzion/src/AppDelegate/AppDelegate.php
+++ api/v1/lib/Oxzion/src/AppDelegate/AppDelegate.php
@@ -2,9 +2,10 @@
 namespace Oxzion\AppDelegate;
 
 use Oxzion\Db\Persistence\Persistence;
+use Zend\Log\Logger;
 
 interface AppDelegate
 {
-    public function setLogger($logger);
+    public function setLogger(Logger $logger);
     public function execute(array $data, Persistence $persistenceService=null);
 }
