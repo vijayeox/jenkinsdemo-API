6460605d177bb1411bfaa9596621fd83b044481f
bharatg <bharatg@myvamla.com>
2019-08-29 17:21:38 +0530

    UI Changes for Dive Insurance App

--- clients/Dive Insurance/view/apps/Dive Insurance/index.js
+++ clients/Dive Insurance/view/apps/Dive Insurance/index.js
@@ -3,37 +3,42 @@ import osjs from 'osjs';
 import React from "react";
 import ReactDOM from "react-dom";
 import merge from "deepmerge";
-import {name as applicationName,appId as application_id} from './metadata.json';
-import {LeftMenuTemplate} from "@oxzion/gui";
-// let menus = [];
+import {name as applicationName,appId as application_id,formId} from './metadata.json';
+import {LeftMenuTemplate,FormRender} from "@oxzion/gui";
 // Our launcher
 const register = (core, args, options, metadata) => {
   // Create a new Application instance
   const proc = core.make('osjs/application', {args, options, metadata});
-
-//   const getMenulist = async () => {
-//     let helper = core.make('oxzion/restClient');
-//     let menulist = await helper.request('v1','/app/'+application_id+'/menu', {}, 'get' );
-//     return menulist;
-// };
-
-// getMenulist().then(response => {
-//  menus = response["data"];
-//  console.log("Menuss");
-//   console.log(menus);
-// });
-// console.log("Menus List");
-// console.log(menus);
-  // Create  a new Window instance
-  proc.createWindow({
+  const user = core.make('osjs/auth').user();
+  let win = proc.createWindow({
     id: 'HubWindow',
     title: metadata.title.en_EN,
     dimension: {width: 400, height: 400},
     position: {left: 700, top: 200}
-  })
-    .on('destroy', () => proc.destroy())
-    .render($content => ReactDOM.render(<LeftMenuTemplate args={core} appId={application_id}/>, $content));
-
+  }).on('destroy', () => proc.destroy());
+  const getCacheData = async () => {
+    let helper = core.make('oxzion/restClient');
+    let cacheData = await helper.request('v1','/app/'+application_id+'/cache', {}, 'get' );
+    return cacheData;
+  };
+  const getFormData = async (workflow_id) => {
+    let helper = core.make('oxzion/restClient');
+    let cacheData = await helper.request('v1','/app/'+application_id+'/workflow/'+workflow_id+'/startform', {}, 'get' );
+    return cacheData;
+  };
+  let appContent = null;
+  getCacheData().then(response => {
+    console.log(response);
+   if(response.data){
+    getFormData(response.data.workflow_id).then(cacheResponse => {
+      if(cacheResponse){
+        win.render($content => ReactDOM.render(<div className='formContent'><FormRender core={core} page={response.data.page} appId={application_id} formId={cacheResponse.data[0].id} content={cacheResponse.data[0].content} data={response.data}/></div>, $content));
+      }
+      });
+   } else {
+    win.render($content => ReactDOM.render(<LeftMenuTemplate core={core} appId={application_id}/>, $content));
+   }
+ });
    return proc;
 };
 
--- clients/Dive Insurance/view/apps/Dive Insurance/webpack.config.js
+++ clients/Dive Insurance/view/apps/Dive Insurance/webpack.config.js
@@ -43,15 +43,13 @@ module.exports = {
           loader: "file-loader",
         }]
       },
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        include: /typeface/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "fonts/[name].[ext]"
-          }
-        }
+       { 
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "url-loader?limit=10000&mimetype=application/font-woff" 
+      },
+      { 
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "file-loader"
       },
       {
         test: /\.(sa|sc|c)ss$/,
