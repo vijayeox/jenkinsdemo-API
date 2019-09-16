26234baa51cfc72a6cbe00324dc84de202790d0f
ravi <ravi@firespike.in>
2019-09-16 21:50:42 +0530

    Added executeQueryWithBindParameters method.

--- api/v1/module/Analytics/src/Service/DashboardService.php
+++ api/v1/module/Analytics/src/Service/DashboardService.php
@@ -14,12 +14,11 @@ use Exception;
 
 class DashboardService extends AbstractService
 {
-
     private $table;
 
-    public function __construct($config, $dbAdapter, DashboardTable $table)
+    public function __construct($config, $dbAdapter, DashboardTable $table, $logger)
     {
-        parent::__construct($config, $dbAdapter);
+        parent::__construct($config, $dbAdapter, $logger);
         $this->table = $table;
     }
 
@@ -54,23 +53,46 @@ class DashboardService extends AbstractService
 
     public function updateDashboard($uuid, &$data)
     {
-        $obj = $this->table->getByUuid($uuid, array());
+//---------------------------------------
+//TODO: Handle concurrency
+//---------------------------------------
+        $updatableColumns = ['name', 'ispublic', 'description', 'dashboard_type', 'isdeleted', 'content'];
+        $selectQuery = 'select ' . join(',', $updatableColumns) . ', id, if(created_by=:user, true, false) as is_owner from ox_dashboard where org_id=:organization and uuid=:uuid and (ispublic=true or created_by=:user)';
+        $selectQueryParams = array('user' => AuthContext::get(AuthConstants::USER_ID),
+                            'organization' => AuthContext::get(AuthConstants::ORG_ID), 
+                            'uuid' => $uuid);
+        try {
+            $selectRs = $this->executeQueryWithBindParameters($selectQuery, $selectQueryParams)->toArray();
+            if (count($selectRs) == 0) {
+                return 0;
+            }
+        }
+        catch(Exception $e) {
+            $this->logger->err($e);
+        }
+
+        $updateQuery = '';
+
+        $obj = $this->table->getByUuid($uuid, array('org_id' => AuthContext::get(AuthConstants::ORG_ID), 'isdeleted' => false));
         if (is_null($obj)) {
             return 0;
         }
         $form = new Dashboard();
-        $data = array_merge($obj->toArray(), $data);
+        $data = array_merge($obj->toArray(), $data); //This is dangerous! What if I send a changed UUID? It gets saved. I can update any column except primary key.
         $form->exchangeArray($data);
         $form->validate();
+        $this->beginTransaction();
         $count = 0;
         try {
             $count = $this->table->save($form);
             if ($count == 0) {
-                $this->rollback();
-                return 0;
+                $this->logger->warn("Nothing was updated because data did not change!");
+                $count = 1; //Fake one row was changed though the row was not updated because data did not change.
             }
+            $this->commit();
         } catch (Exception $e) {
             $this->rollback();
+            $this->logger->err($e);
             return 0;
         }
         return $count;
@@ -108,23 +130,29 @@ class DashboardService extends AbstractService
 
     public function getDashboard($uuid)
     {
-        $id = $this->getIdFromUuid('ox_dashboard',$uuid);
         try{
-            $query = "Select ox_dashboard.uuid,ox_dashboard.name,ox_dashboard.ispublic,ox_dashboard.description, ox_dashboard.dashboard_type,IF(ox_dashboard.created_by = ".AuthContext::get(AuthConstants::USER_ID).", 'true', 'false') as is_owner,ox_dashboard.org_id,ox_dashboard.isdeleted, ox_widget_dashboard_mapper.widget_id from ox_dashboard INNER JOIN ox_widget_dashboard_mapper on ox_dashboard.id = ox_widget_dashboard_mapper.dashboard_id where ox_dashboard.isdeleted <> 1 AND ox_widget_dashboard_mapper.id =".$id;
-            $response = $this->executeQuerywithParams($query)->toArray();
+            $query = "select uuid, name, ispublic, description, dashboard_type, date_created, content, if(created_by=?, true, false) as is_owner from ox_dashboard where org_id=? and uuid=? and (ispublic=true or created_by=?)";
+            $queryParams = array(AuthContext::get(AuthConstants::USER_ID),
+                            AuthContext::get(AuthConstants::ORG_ID), 
+                            $uuid, 
+                            AuthContext::get(AuthConstants::USER_ID));
+            $resultSet = $this->executeQueryWithBindParameters($query, $queryParams)->toArray();
+            if (count($resultSet) == 0) {
+                return 0;
+            }
+            $response = [
+                'dashboard' => $resultSet[0]
+            ];
+            return $response;
         }
         catch (Exception $e) {
+            $this->logger->err($e);
             return 0;
         }
-        if (count($response) == 0) {
-            return 0;
-        }
-        return $response[0];
     }
 
     public function getDashboardList($params = null)
     {
-
             $paginateOptions = FilterUtils::paginate($params);
             $where = $paginateOptions['where'];
             $where .= empty($where) ? "WHERE ox_dashboard.isdeleted <> 1 AND (ox_dashboard.org_id =".AuthContext::get(AuthConstants::ORG_ID).") and (ox_dashboard.created_by = ".AuthContext::get(AuthConstants::USER_ID)." OR ox_dashboard.ispublic = 1)" : " AND ox_dashboard.isdeleted <> 1 AND (ox_dashboard.org_id =".AuthContext::get(AuthConstants::ORG_ID).") and (ox_dashboard.created_by = ".AuthContext::get(AuthConstants::USER_ID)." OR ox_dashboard.ispublic = 1)";
@@ -147,3 +175,4 @@ class DashboardService extends AbstractService
                      'total' => $count);
     }
 }
+
