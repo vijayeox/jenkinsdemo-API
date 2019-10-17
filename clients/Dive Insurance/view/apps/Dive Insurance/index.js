72e26b05009fd78f302cb89fe5e78ffd101ad744
Prajwa K <prajwal.k1@gmail.com>
2019-10-17 16:00:13 +0530

    Added react wrapper for Form Render and LeftMenuTemplate

--- clients/Dive Insurance/view/apps/Dive Insurance/index.js
+++ clients/Dive Insurance/view/apps/Dive Insurance/index.js
@@ -1,21 +1,14 @@
-import "./index.scss";
 import osjs from "osjs";
+import { name as applicationName } from "./metadata.json";
 import React from "react";
 import ReactDOM from "react-dom";
-import merge from "deepmerge";
-import {
-  name as applicationName,
-  appId as application_id,
-  formId
-} from "./metadata.json";
-import LeftMenuTemplate from "OxzionGUI/LeftMenuTemplate";
-import FormRender from "OxzionGUI/components/App/FormRender";
+import Home from "./home";
+
 // Our launcher
 const register = (core, args, options, metadata) => {
   // Create a new Application instance
   const proc = core.make("osjs/application", { args, options, metadata });
-  const user = core.make("osjs/auth").user();
-  let win = proc
+  proc
     .createWindow({
       id: "HubWindow",
       title: metadata.title.en_EN,
@@ -28,100 +21,9 @@ const register = (core, args, options, metadata) => {
       },
       position: { left: 700, top: 200 }
     })
-    .on("destroy", () => proc.destroy());
-  var cacheId;
-  const getCacheData = async () => {
-    let helper = core.make("oxzion/restClient");
-    let cacheData = await helper.request(
-      "v1",
-      "/app/" + application_id + "/cache",
-      {},
-      "get"
-    );
-    return cacheData;
-  };
-  const deleteCacheData = async () => {
-    let helper = core.make("oxzion/restClient");
-    let cacheData = await helper.request(
-      "v1",
-      "/app/" + application_id + "/deletecache",
-      {},
-      "delete"
-    );
-    return cacheData;
-  };
-  const updateCacheData = async cacheId => {
-    let helper = core.make("oxzion/restClient");
-    let cacheData = await helper.request(
-      "v1",
-      "/app/" + application_id + "/cache" + cacheId,
-      {},
-      "delete"
-    );
-    return cacheData;
-  };
-  const getFormData = async workflow_id => {
-    let helper = core.make("oxzion/restClient");
-    let cacheData = await helper.request(
-      "v1",
-      "/app/" + application_id + "/workflow/" + workflow_id + "/startform",
-      {},
-      "get"
-    );
-    return cacheData;
-  };
+    .on("destroy", () => proc.destroy())
+    .render($content => ReactDOM.render(<Home args={core} />, $content));
 
-  const postSubmitCallback = data => {
-    deleteCacheData().then(response => {
-      if (response.status == "success") {
-        proc.destroy();
-        core.run("DiveInsurance");
-      }
-    });
-  };
-
-  getCacheData().then(cacheResponse => {
-    var cache = cacheResponse.data;
-    if (cache && cache.data) {
-      if (cache.data.workflow_uuid) {
-        getFormData(cache.data.workflow_uuid).then(formResponse => {
-          if (formResponse && formResponse.data.length > 0) {
-            cacheId = cacheResponse["id"];
-            win.render($content => {
-              ReactDOM.render(
-                <div className="formContent">
-                  <FormRender
-                    postSubmitCallback={postSubmitCallback}
-                    core={core}
-                    page={cache.data.page}
-                    appId={application_id}
-                    formId={formResponse.data[0].id}
-                    content={JSON.parse(formResponse.data[0].content)}
-                    data={cache.data}
-                  />
-                </div>,
-                $content
-              );
-            });
-          } else {
-            win.render($content =>
-              ReactDOM.render(
-                <LeftMenuTemplate core={core} appId={application_id} />,
-                $content
-              )
-            );
-          }
-        });
-      }
-    } else {
-      win.render($content =>
-        ReactDOM.render(
-          <LeftMenuTemplate core={core} appId={application_id} />,
-          $content
-        )
-      );
-    }
-  });
   return proc;
 };
 
