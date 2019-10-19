11bf37e7a85e0d3e3f0f4acda11fe65ca0bfac40
Rajesh S <rajesha.s@gmail.com>
2019-10-19 16:11:41 +0530

    adding log config

---
+++ api/v1/config/autoload/log.php
@@ -0,0 +1,49 @@
+<?php
+
+    return [ 
+        'logger' => [
+                'rootLogger' => [
+                    'level' => 'INFO',
+                    'appenders' => ['default']
+                ],
+                'loggers' => [
+                    'ControllerLogger' => [
+                        'level' => 'INFO',
+                        'appenders' => ['request']
+                    ],
+                    'OxzionLogger' => [
+                        'level' => 'INFO',
+                        'appenders' => ['default']
+                    ]
+                ],
+                'appenders' => [
+                    'default' => [
+                        'class' => 'LoggerAppenderDailyFile',
+                        'layout' => [
+                            'class' => 'LoggerLayoutPattern',
+                            'params' => [
+                                'conversionPattern' => '%date{d/m/Y H:i:s,u} %C->%M %-5level [%pid] %msg%n%ex'
+                            ]
+                        ],
+                        'params' => [
+                            'file' => __DIR__."/../../logs/application.log",
+                            'append' => true
+                        ]
+                    ],
+                    'request' => [
+                        'class' => 'LoggerAppenderDailyFile',
+                        'layout' => [
+                            'class' => 'LoggerLayoutPattern',
+                            'params' => [
+                                'conversionPattern' => '%date(d:m:Y H:i:s,u) [%pid] From:%server{REMOTE_ADDR}:%server{REMOTE_PORT} Request:[%request]%n%ex'
+                            ]
+                        ],
+                        'params' => [
+                            'file' => __DIR__."/../../logs/request.log",
+                            'append' => true
+                        ]
+                    ]
+                ]
+            ]
+        ];
+?>
