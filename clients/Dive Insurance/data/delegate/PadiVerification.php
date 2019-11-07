4e7db5ea21288039032319c5a4ae98970175e617
brian <brianmp@myvamla.com>
2019-11-07 12:03:29 +0530

    Fixed issue with merging arrays for initial to be displayed in padiVerification form

--- clients/Dive Insurance/data/delegate/PadiVerification.php
+++ clients/Dive Insurance/data/delegate/PadiVerification.php
@@ -20,15 +20,14 @@ class PadiVerification extends AbstractAppDelegate
         if(!isset($data['member_number'])){
             return;
         }
-        $select = "Select firstname, MI as initial, lastname, email, address1, address2, city, state, country_code, zip, home_phone, work_phone, num as mobilephone FROM padi_data WHERE member_number ='".$data['member_number']."'";  
+        $select = "Select firstname, MI as initial, lastname, email, address1, address2, city, state, country_code, zip, home_phone, work_phone, num as mobilephone FROM padi_data WHERE member_number ='".$data['member_number']."'";
         $result = $persistenceService->selectQuery($select);
         if($result->count() > 0){
             $response = array();
             while ($result->next()) {
                 $response[] = $result->current();
             }
-            
-            $selectQuery = "Select state FROM state_license WHERE state_in_short ='".$response[0]['state']."'";  
+            $selectQuery = "Select state FROM state_license WHERE state_in_short ='".$response[0]['state']."'";
             $resultSet = $persistenceService->selectQuery($selectQuery);
             $stateDetails = array();
             while ($resultSet->next()) {
@@ -37,7 +36,11 @@ class PadiVerification extends AbstractAppDelegate
             if(isset($stateDetails)){
                 $response[0]['state'] = $stateDetails[0]['state'];
             }
-
+            if(isset($response[0]['initial']))
+            {
+                $response[0]['MI'] = $response[0]['initial'];
+                unset($response[0]['initial']);
+            }
             if(isset($response[0]['country_code'])){
                 $response[0]['country'] = Country::codeToCountryName($response[0]['country_code']);
             }
