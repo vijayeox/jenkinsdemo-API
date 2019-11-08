08c7c452dbd2ece75cd6893f67995aa38d0c7808
prajwalv <prajwalv@myvamla.com>
2019-11-08 15:48:29 +0530

    Calender bug, event was not getting added.

--- api/v1/lib/Oxzion/src/Messaging/Constants.php
+++ api/v1/lib/Oxzion/src/Messaging/Constants.php
@@ -3,7 +3,7 @@ namespace Oxzion\Messaging;
 
 class Constants
 {
-    const CONNECTION_HOST = 'tcp://localhost:61613';
+    const CONNECTION_HOST = 'tcp://172.16.1.84:61613';
     const MAIL = "mail";
     const WEBSOCKET = "websocket";
 }
