c8e1eff890c6b871a151d8b0602875d785b9a48e
purvi <purvi@myvamla.com>
2019-11-18 13:50:31 +0530

    Added eoxapps folder as a template application.

---
+++ api/v1/data/eoxapps/home.js
@@ -0,0 +1,128 @@
+import React from "react";
+import { appId as application_id } from "./metadata.json";
+import LeftMenuTemplate from "OxzionGUI/LeftMenuTemplate";
+import FormRender from "OxzionGUI/components/App/FormRender";
+import "./index.scss";
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.core = this.props.args;
+    this.helper = this.core.make("oxzion/restClient");
+    this.state = {
+      cacheID: undefined,
+      formContent: undefined,
+      cachePage: undefined,
+      formID: undefined,
+      cacheData: undefined,
+      showMenuPage: undefined
+    };
+  }
+
+  componentDidMount() {
+    this.getCacheData().then(cacheResponse => {
+      var cache = cacheResponse.data;
+      if (cache && cache.data) {
+        if (cache.data.workflow_uuid) {
+          this.getFormData(cache.data.workflow_uuid).then(formResponse => {
+            if (formResponse && formResponse.data.length > 0) {
+              this.setState({
+                formContent: JSON.parse(formResponse.data[0].content),
+                cachePage: cache.data.page,
+                formID: formResponse.data[0].id,
+                cacheData: cacheResponse.data
+              });
+            } else {
+              this.setState({
+                showMenuPage: true
+              });
+            }
+          });
+        } else {
+          this.setState({
+            showMenuPage: true
+          });
+        }
+      } else {
+        this.setState({
+          showMenuPage: true
+        });
+      }
+    });
+  }
+
+  async getCacheData() {
+    let cacheData = await this.helper.request(
+      "v1",
+      "/app/" + application_id + "/cache",
+      {},
+      "get"
+    );
+    return cacheData;
+  }
+
+  async deleteCacheData() {
+    let cacheData = await this.helper.request(
+      "v1",
+      "/app/" + application_id + "/deletecache",
+      {},
+      "delete"
+    );
+    return cacheData;
+  }
+
+  async updateCacheData() {
+    let cacheData = await this.helper.request(
+      "v1",
+      "/app/" + application_id + "/deletecache",
+      {},
+      "delete"
+    );
+    return cacheData;
+  }
+
+  async getFormData(workflow_id) {
+    let formData = await this.helper.request(
+      "v1",
+      "/app/" + application_id + "/workflow/" + workflow_id + "/startform",
+      {},
+      "get"
+    );
+    return formData;
+  }
+
+  postSubmitCallback = data => {
+    this.deleteCacheData().then(response => {
+      if (response.status == "success") {
+        this.setState({
+          formContent: undefined,
+          formID: undefined,
+          showMenuPage: true
+        });
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div style={{ height: "inherit" }}>
+        {this.state.formContent ? (
+          <div className="formContent">
+            <FormRender
+              postSubmitCallback={this.postSubmitCallback}
+              core={this.core}
+              page={this.state.cachePage}
+              appId={application_id}
+              formId={this.state.formID}
+              content={this.state.formContent}
+              data={this.state.cacheData.data}
+            />
+          </div>
+        ) : this.state.showMenuPage ? (
+          <LeftMenuTemplate core={this.core} appId={application_id} />
+        ) : null}
+      </div>
+    );
+  }
+}
+export default Home;
