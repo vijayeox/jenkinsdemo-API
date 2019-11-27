d3b13dad5795bbe5418c0f9f44c489e50ee8e73e
Rajesh S <rajesha.s@gmail.com>
2019-11-27 17:43:41 +0530

    Fixed failing tests

--- api/v1/module/Privilege/test/Controller/PrivilegeControllerTest.php
+++ api/v1/module/Privilege/test/Controller/PrivilegeControllerTest.php
@@ -118,6 +118,6 @@ class PrivilegeControllerTest extends MainControllerTest
         $this->assertMatchedRouteName('getMasterPrivilege');
         $content = (array)json_decode($this->getResponse()->getContent(), true);
         $this->assertEquals($content['status'], 'success');
-        $this->assertEquals(count($content['data']['masterPrivilege']),28);
+        $this->assertEquals(count($content['data']['masterPrivilege']),23);
     }
 }
