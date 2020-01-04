2859b0c4b3bf60f6fccbc0a90a2d01d8b4a0dc6c
nehar <neha@myvamla.com>
2020-01-04 13:21:21 +0530

    Changes to PadiVerification

--- clients/DiveInsurance/data/delegate/PadiVerification.php
+++ clients/DiveInsurance/data/delegate/PadiVerification.php
@@ -17,33 +17,63 @@ class PadiVerification extends AbstractAppDelegate
         if(isset($data['padi'])){
             $data['member_number'] = $data['padi'];
         }
+        if(isset($data['business_padi'])){
+            $data['member_number'] = $data['business_padi'];
+        }
         if(!isset($data['member_number'])){
             return;
         }
-        $select = "Select firstname, MI as initial, lastname, email, address1, address2, city, state, country_code, zip, home_phone, work_phone, num as mobilephone FROM padi_data WHERE member_number ='".$data['member_number']."'";
+        if(isset($data['padi']) && !isset($data['business_padi'])){
+            $select = "Select firstname, MI as initial, lastname, email, address1, address2, city, state, country_code, zip, home_phone, work_phone, num as mobilephone FROM padi_data WHERE member_number ='".$data['member_number']."'";
+
+        }else if(isset($data['business_padi'])){
+            $select = "Select business_name, email as business_email, address1 as business_address1, address2 as business_address2, city as business_city, state as business_state, country_code as business_country_code, zip as business_zip, home_phone as business_home_phone, work_phone as business_work_phone, num as business_mobilephone FROM padi_data WHERE member_number ='".$data['member_number']."'";
+        }
         $result = $persistenceService->selectQuery($select);
         if($result->count() > 0){
             $response = array();
             while ($result->next()) {
                 $response[] = $result->current();
             }
-            $selectQuery = "Select state FROM state_license WHERE state_in_short ='".$response[0]['state']."'";
+
+            if(isset($data['padi']) && !isset($data['business_padi'])){
+              $selectQuery = "Select state FROM state_license WHERE state_in_short ='".$response[0]['state']."'";
+
+            }else if(isset($data['business_padi'])){
+              $selectQuery = "Select state FROM state_license WHERE state_in_short = '".$response[0]['business_state']."'";
+            }
+
             $resultSet = $persistenceService->selectQuery($selectQuery);
             $stateDetails = array();
             while ($resultSet->next()) {
                 $stateDetails[] = $resultSet->current();
-            }
+            }       
             if(isset($stateDetails) && count($stateDetails)>0){
-                $response[0]['state'] = $stateDetails[0]['state'];
+                if(isset($response[0]['business_state'])){
+                    $response[0]['business_state'] = $stateDetails[0]['state'];
+                }else{
+                    $response[0]['state'] = $stateDetails[0]['state'];
+                }
             }
-            if(isset($response[0]['country_code'])){
+            
+            if(isset($response[0]['business_country_code'])){
+                $response[0]['business_country'] = Country::codeToCountryName($response[0]['business_country_code']);
+            }else if(isset($response[0]['country_code'])){
                 $response[0]['country'] = Country::codeToCountryName($response[0]['country_code']);
             }
             $returnArray = array_merge($data,$response[0]);
-            $returnArray['padiVerified'] = true;
+            if(isset($data['padi']) && !isset($data['business_padi'])){
+               $returnArray['padiVerified'] = true;
+            }else if(isset($data['business_padi'])){
+               $returnArray['businessPadiVerified'] = true;
+            }
             return $returnArray;
         } else {
-            $returnArray['padiVerified'] = false;
+            if(isset($data['padi']) && !isset($data['business_padi'])){
+                $returnArray['padiVerified'] = false;
+            }else if(isset($data['business_padi'])){
+                $returnArray['businessPadiVerified'] = false;
+            }
             $data = array_merge($data,$returnArray);
             return $data;
         }
