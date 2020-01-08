c58602e86dba2a1a6ce21ce486cb4c894b589536
gudduk <gudduk@myvamlia.com>
2020-01-08 17:30:23 +0530

    commit and revert

--- api/v1/module/App/config/module.config.php
+++ api/v1/module/App/config/module.config.php
@@ -70,17 +70,6 @@ return [
                     ],
                 ],
             ],
-            'ftpClient' => [
-                'type' => Segment::class,
-                'options' => [
-                    'route' => '/app/ftps',
-                    'defaults' => [
-                        'controller' => Controller\AppController::class,
-                        'action' => 'uploadFileToFTP',
-                        'method' => 'GET'
-                    ],
-                ],
-            ],
             'appupload' => [
                 'type' => Segment::class,
                 'options' => [
