e541c95625170a03b7f1eb2c1aba12da60b099c7
nehar <neha@myvamla.com>
2020-01-15 14:16:36 +0530

    Changes to Auto Renewal Rate Card

--- clients/DiveInsurance/data/delegate/AutoRenewalRateCard.php
+++ clients/DiveInsurance/data/delegate/AutoRenewalRateCard.php
@@ -10,15 +10,14 @@ require_once __DIR__."/RateCard.php";
 class AutoRenewalRateCard extends RateCard{
     public function __construct(){
         parent::__construct();
-    //     $this->template = array(
-    //     'Individual Professional Liability' 
-    //         => array('documents'));
-    // }
+        $this->unsetVariables = array('Individual Professional Liability' => array('workflowInstanceId','policy_id','certificate_no','start_date','end_date','documents'));
+    }
 
     // Premium Calculation values are fetched here
     public function execute(array $data,Persistence $persistenceService)
     {  
         $this->logger->info("AutoRenewal Rate Card");
+        $this->cleanData($data);
         $startYear = date("Y");
         $endYear = date("Y") + 1;
         $policy_period = "July 01,".$startYear." - June 30,".$endYear;
@@ -27,6 +26,7 @@ class AutoRenewalRateCard extends RateCard{
         $date_range = array("label" => $policy_period,"value" => $data['start_date']);
         $data['start_date_range'] = $date_range;
         $this->logger->info("AUTO RATE CARD PERSISTENCE".print_r($data,true));
+
         $data = parent::execute($data,$persistenceService);
 
         $this->logger->info("PRESENT RATE card".print_r($data,true));
@@ -49,16 +49,16 @@ class AutoRenewalRateCard extends RateCard{
     }
 
 // Data Cleanup is done here
-    // private function cleanData(&$data){
-    //     $this->logger->info("CLEAN DATA");
-    //     $unsetVar = $this->unsetVariables[$data['product']];
-    //     $this->logger->info("UNSET VARIABLES".print_r($unsetVar,true));
-    //     for($i=0;$i< sizeof($unsetVar);$i++){
-    //         $this->logger->info("CLEAN DATA FOR");
-    //         if(isset($data[$unsetVar[$i]])){
-    //             unset($data[$unsetVar[$i]]);
-    //         }
-    //     }
-    //     $this->logger->info("CLEAN DATA END".print_r($data,true));
-    // }
+    private function cleanData(&$data){
+        $this->logger->info("CLEAN DATA");
+        $unsetVar = $this->unsetVariables[$data['product']];
+        $this->logger->info("UNSET VARIABLES".print_r($unsetVar,true));
+        for($i=0;$i< sizeof($unsetVar);$i++){
+            $this->logger->info("CLEAN DATA FOR");
+            if(isset($data[$unsetVar[$i]])){
+                unset($data[$unsetVar[$i]]);
+            }
+        }
+        $this->logger->info("CLEAN DATA END".print_r($data,true));
+    }
 }
