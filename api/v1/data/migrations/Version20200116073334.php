d5e58ae1957d626ce5d85fb0e691fda40bbb9755
Rajesh S <rajesha.s@gmail.com>
2020-01-16 17:53:56 +0530

    removed the foreign key for workflow_instance_id as it was causing problems in starting the workflow

--- api/v1/data/migrations/Version20200116073334.php
+++ api/v1/data/migrations/Version20200116073334.php
@@ -22,7 +22,6 @@ final class Version20200116073334 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql("ALTER TABLE ox_file ADD CONSTRAINT FK_orgid FOREIGN KEY (org_id) REFERENCES ox_organization(id)");
         $this->addSql("ALTER TABLE ox_file MODIFY workflow_instance_id INTEGER(32)");
-        $this->addSql("ALTER TABLE ox_file ADD CONSTRAINT FK_wfInstanceid FOREIGN KEY (workflow_instance_id) REFERENCES ox_workflow_instance(id)");
         $this->addSql("ALTER TABLE ox_file ADD UNIQUE (uuid)");
         $this->addSql("ALTER TABLE ox_file ADD CONSTRAINT FK_modifiedBy FOREIGN KEY (modified_by) REFERENCES ox_user(id)");
         $this->addSql("ALTER TABLE ox_file ADD CONSTRAINT FK_createdBy FOREIGN KEY (created_by) REFERENCES ox_user(id)");
@@ -34,7 +33,6 @@ final class Version20200116073334 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql("ALTER TABLE `ox_file` DROP FOREIGN KEY FK_orgid");
-        $this->addSql("ALTER TABLE ox_file DROP FOREIGN KEY FK_wfInstanceid");
         $this->addSql("ALTER TABLE ox_file DROP FOREIGN KEY FK_fieldId");
         $this->addSql("ALTER TABLE ox_file DROP FOREIGN KEY FK_modifiedBy");
         $this->addSql("ALTER TABLE ox_file DROP FOREIGN KEY FK_createdBy");
