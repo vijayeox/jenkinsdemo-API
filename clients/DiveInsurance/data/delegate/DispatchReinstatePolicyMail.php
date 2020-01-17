b525c1a1a681a59c9b1b275b8971a54184e4ad5f
purvi <purvi@myvamla.com>
2020-01-17 18:51:29 +0530

    dispatch policy delegate change

--- clients/DiveInsurance/data/delegate/DispatchReinstatePolicyMail.php
+++ clients/DiveInsurance/data/delegate/DispatchReinstatePolicyMail.php
@@ -1,31 +1,37 @@
 <?php
 use Oxzion\AppDelegate\MailDelegate;
+use Oxzion\Db\Persistence\Persistence;
+use Oxzion\Messaging\MessageProducer;
+use Oxzion\Encryption\Crypto;
+require_once __DIR__."/DispatchNotification.php";
 
-class DispatchReinstatePolicyMail extends MailDelegate
+class DispatchReinstatePolicyMail extends DispatchNotification
 {
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    public function setDocumentPath($destination)
-    {
-        $this->destination = $destination;
+    public $template;
+ 
+    public function __construct(){
         $this->template = array(
             'Individual Professional Liability' => 'CancelPolicyMailTemplate',
             'Dive Boat' => 'CancelPolicyMailTemplate',
             'Dive Store' => 'CancelPolicyMailTemplate');
+        parent::__construct();
+        
     }
 
-    protected function dispatch(array $data)
+    protected function execute(array $data)
     {
-        $this->logger->info("DISPATCH DATA" . print_r($data, true));
-        $mailOptions = array();
-        $mailOptions['to'] = $data['email'];
-        $mailOptions['subject'] = $data['subject'];
+        $this->logger->info("Dispatch reinstate policy mail notification" .json_encode($data));
         $data['template'] = $this->template[$data['product']];
-        $response = $this->sendMail($data, $data['template'], $mailOptions);
+        $data['subject'] = 'Policy Auto Renewal';
+        $response = $this->dispatch($data);
+        $data['userCancellationReason'] = '';
+        $data['othersCsr'] = '';
+        $data['reinstateAmount'] = '';
+        $data['csrCancellationReason'] = '';
+        $data['cancellationStatus'] = '';
+        $data['reasonforRejection'] = '';
+        $data['othersUser'] = '';
         return $response;
     }
 }
