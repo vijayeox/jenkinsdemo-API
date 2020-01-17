823887a79a4b442f27598f38e3ebccc3979bfbfa
nehar <neha@myvamla.com>
2020-01-17 13:50:19 +0530

    Added File Trait/Refactored File Delegate

---
+++ api/v1/lib/Oxzion/src/AppDelegate/FileTrait.php
@@ -0,0 +1,34 @@
+<?php
+namespace Oxzion\AppDelegate;
+
+use Oxzion\Db\Persistence\Persistence;
+use Oxzion\Service\FileService;
+use Logger;
+
+trait FileTrait
+{
+    protected $logger;
+    protected $fileService;
+    protected $appId;
+    
+    public function __construct(){
+        $this->logger = Logger::getLogger(__CLASS__);
+    }
+    public function setFileService(FileService $fileService){
+        $this->logger->info("SET FILE SERVICE");
+        $this->fileService = $fileService;
+    }
+    public function setAppId($appId){
+        $this->logger->info("SET APP ID");
+        $this->appId = $appId;
+    }
+    protected function getFileList($params,$filterparams = null){
+        $this->logger->info("SET FILE LIST");
+        return $this->fileService->getFileList($this->appId,$params,$filterparams);
+    }
+
+    protected function saveFile($params,$fileId){
+        $this->logger->info("SAVE FILE");
+        return $this->fileService->updateFile($params,$fileId);
+    }
+}
