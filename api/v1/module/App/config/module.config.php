947dd3590670607cd3ce1a0033b722092f7ac7d9
purvi <purvi@myvamla.com>
2020-01-24 11:47:19 +0530

    Added Completed Status for Manage Policies to not show any other Policy Status Records in search page and route.

--- api/v1/module/App/config/module.config.php
+++ api/v1/module/App/config/module.config.php
@@ -519,7 +519,7 @@ return [
             'filelistfilter' => [
                 'type' => Segment::class,
                 'options' => [
-                    'route' => '/app/:appId/file/search',
+                    'route' => '/app/:appId/file/search[/status/:workflowStatus]',
                     'defaults' => [
                         'controller' => Controller\FileController::class,
                         'method' => 'GET',
