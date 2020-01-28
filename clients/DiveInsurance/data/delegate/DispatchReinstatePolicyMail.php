5430099f4ee7cef7fa9b1b71d9e489b48b0c0bc3
bharatg <bharatg@myvamla.com>
2020-01-28 18:16:13 +0530

    Changed the reinstate mail delegate to send documents along with the mail. minor changes in bpmn and cancel policy.

--- clients/DiveInsurance/data/delegate/DispatchReinstatePolicyMail.php
+++ clients/DiveInsurance/data/delegate/DispatchReinstatePolicyMail.php
@@ -4,10 +4,14 @@ use Oxzion\AppDelegate\MailDelegate;
 use Oxzion\Db\Persistence\Persistence;
 use Oxzion\Messaging\MessageProducer;
 use Oxzion\Encryption\Crypto;
-require_once __DIR__."/DispatchNotification.php";
+use Oxzion\Utils\FileUtils;
+use Oxzion\Utils\ArtifactUtils;
+require_once __DIR__."/DispatchDocument.php";
 
-class DispatchReinstatePolicyMail extends DispatchNotification
+class DispatchReinstatePolicyMail extends DispatchDocument
 { 
+    public $template;
+    
     public function __construct(){
         $this->template = array(
             'Individual Professional Liability' => 'ReinstatePolicyMailTemplate',
@@ -15,23 +19,44 @@ class DispatchReinstatePolicyMail extends DispatchNotification
             'Emergency First Response' => 'ReinstatePolicyMailTemplate',
             'Dive Store' => 'ReinstatePolicyMailTemplate');
         parent::__construct();
-        
     }
 
     public function execute(array $data, Persistence $persistenceService)
     {
-        $this->logger->info("Dispatch reinstate policy mail notification");
-        $data['template'] = $this->template[$data['product']];
-        $data['subject'] = 'Your Policy has been Reinstated!';
-        $response = $this->dispatch($data);
-        $data['userCancellationReason'] = '';
-        $data['othersCsr'] = '';
-        $data['reinstateAmount'] = '';
-        $data['csrCancellationReason'] = '';
-        $data['cancellationStatus'] = '';
-        $data['reasonforRejection'] = '';
-        $data['othersUser'] = '';
-        return $response;
+        $this->logger->info("Dispatch reinstate policy mail notification ".json_encode($data));
+        $temp = $data['reinstateDocuments'];
+        if(!is_array($temp)){
+            $temp = json_decode($data['reinstateDocuments'], true);
+        }
+        $dest = ArtifactUtils::getDocumentFilePath($this->destination,$data['fileId'],array('orgUuid' => $data['orgUuid']));
+        
+        unset($data['documents']);
+        $this->logger->info("Dispatch reinstate policy mail notification - data consists of:".json_encode($temp));
+        foreach($temp as $key => $value){
+            $this->logger->info("The documents consist of : $value");
+            $fileName = basename($value);
+            $this->logger->info("the fileName is: ".print_r($fileName, true));
+            $this->logger->info("the new path is : ".json_encode($dest));
+            $data['documents'][$key] = $dest['relativePath'].$fileName;
+            $this->logger->info("The destination value is : ".print_r($this->destination.$value, true));
+            $this->logger->info("The destination relative path is : ".print_r($this->destination.$dest['relativePath'], true));
+            FileUtils::copy($this->destination.$value, $fileName, $this->destination.$dest['relativePath']);
+        }
+        $this->logger->info("the document array consists of : ".print_r($data['documents'], true));
+        unset($data['reinstateDocuments']);
+        unset($data['userCancellationReason']);
+        unset($data['othersCsr']);
+        unset($data['reinstateAmount']);
+        unset($data['reasonforCsrCancellation']);
+        unset($data['cancellationStatus']);
+        unset($data['reasonforRejection']);
+        unset($data['othersUser']);
+        $temp = $data;
+        $temp['template'] = $this->template[$data['product']];
+        $temp['subject'] = 'Your Policy has been Reinstated!';
+        $response = $this->dispatch($temp);
+        $this->logger->info("Dispatch reinstate policy returning data --- ".json_encode($data));
+        return $data;
     }
 }
 ?>
