a4a0408cb1827e8018bb4b27fecd5a885ccffbd3
danishsultan <danishsultan@myvamla.com>
2020-01-30 19:18:54 +0530

    updated build and deploy scripts to support bridgemed and task app

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -342,11 +342,11 @@ edms()
         echo -e "${GREEN}Copying edms Complete!${RESET}"
     fi
 }
-clients()
+diveinsurance()
 {
     cd ${TEMP}
     echo -e "${YELLOW}Copying EOX apps...${RESET}"
-    if [ ! -d "./clients" ] ;
+    if [ ! -d "./clients/DiveInsurance" ] ;
     then
         echo -e "${RED}EOX Apps was not packaged so skipping it\n${RESET}"
     else
@@ -355,10 +355,68 @@ clients()
         cd ${TEMP}/clients
         echo -e "${YELLOW}Copying EOX Apps to /opt/oxzion/eoxapps directory${RESET}"
         mkdir -p /opt/oxzion/eoxapps
-        rsync -rl . /opt/oxzion/eoxapps/
-        echo -e "${YELLOW}Building Diveinsurance using deployapp API${RESET}"
+        rsync -rl ./DiveInsurance /opt/oxzion/eoxapps
+        echo -e "${YELLOW}Building DiveInsurance apps using deployapp API${RESET}"
         jwt=$(curl --location --request POST 'http://localhost:8080/auth' --form 'username=bharatgtest' --form 'password=password' 2>/dev/null | jq -r '.data.jwt')
         curl --location --request POST 'http://localhost:8080/app/deployapp' -H 'Authorization: Bearer '${jwt}'' -F 'path=/opt/oxzion/eoxapps/DiveInsurance'
+        echo -e "${YELLOW}Copying EOX Apps directory Complete!${RESET}"
+        echo -e "${GREEN}Building and Running package discover in bos${RESET}"
+        cd /opt/oxzion/view/bos/
+        npm run build
+        npm run package:discover
+        chown oxzion:oxzion -R /opt/oxzion/eoxapps
+        chown oxzion:oxzion -R /opt/oxzion/view
+        chmod 777 -R /opt/oxzion/eoxapps
+        systemctl start view
+        echo -e "${YELLOW}Started view service!${RESET}"
+    fi
+}
+task()
+{
+    cd ${TEMP}
+    echo -e "${YELLOW}Copying EOX apps...${RESET}"
+    if [ ! -d "./clients/Task" ] ;
+    then
+        echo -e "${RED}EOX Apps was not packaged so skipping it\n${RESET}"
+    else
+        echo -e "${GREEN}Stopping view service${RESET}"
+        systemctl stop view
+        cd ${TEMP}/clients
+        echo -e "${YELLOW}Copying EOX Apps to /opt/oxzion/eoxapps directory${RESET}"
+        mkdir -p /opt/oxzion/eoxapps
+        rsync -rl ./Task /opt/oxzion/eoxapps
+        echo -e "${YELLOW}Building Task apps using deployapp API${RESET}"
+        jwt=$(curl --location --request POST 'http://localhost:8080/auth' --form 'username=bharatgtest' --form 'password=password' 2>/dev/null | jq -r '.data.jwt')
+        curl --location --request POST 'http://localhost:8080/app/deployapp' -H 'Authorization: Bearer '${jwt}'' -F 'path=/opt/oxzion/eoxapps/Task'
+        echo -e "${YELLOW}Copying EOX Apps directory Complete!${RESET}"
+        echo -e "${GREEN}Building and Running package discover in bos${RESET}"
+        cd /opt/oxzion/view/bos/
+        npm run build
+        npm run package:discover
+        chown oxzion:oxzion -R /opt/oxzion/eoxapps
+        chown oxzion:oxzion -R /opt/oxzion/view
+        chmod 777 -R /opt/oxzion/eoxapps
+        systemctl start view
+        echo -e "${YELLOW}Started view service!${RESET}"
+    fi
+}
+bridgemed()
+{
+    cd ${TEMP}
+    echo -e "${YELLOW}Copying EOX apps...${RESET}"
+    if [ ! -d "./clients/BridgeMed" ] ;
+    then
+        echo -e "${RED}EOX Apps was not packaged so skipping it\n${RESET}"
+    else
+        echo -e "${GREEN}Stopping view service${RESET}"
+        systemctl stop view
+        cd ${TEMP}/clients
+        echo -e "${YELLOW}Copying EOX Apps to /opt/oxzion/eoxapps directory${RESET}"
+        mkdir -p /opt/oxzion/eoxapps
+        rsync -rl ./BridgeMed /opt/oxzion/eoxapps
+        echo -e "${YELLOW}Building BridgeMed apps using deployapp API${RESET}"
+        jwt=$(curl --location --request POST 'http://localhost:8080/auth' --form 'username=bharatgtest' --form 'password=password' 2>/dev/null | jq -r '.data.jwt')
+        curl --location --request POST 'http://localhost:8080/app/deployapp' -H 'Authorization: Bearer '${jwt}'' -F 'path=/opt/oxzion/eoxapps/BridgeMed'
         echo -e "${YELLOW}Copying EOX Apps directory Complete!${RESET}"
         echo -e "${GREEN}Building and Running package discover in bos${RESET}"
         cd /opt/oxzion/view/bos/
@@ -383,7 +441,9 @@ mattermost
 orocrm
 rainloop
 openproject
-clients
+diveinsurance
+task
+bridgemed
 workflow
 helpapp
 #edms
