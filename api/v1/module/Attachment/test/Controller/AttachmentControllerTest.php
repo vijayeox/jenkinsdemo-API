2d0d319fc9d80d4511b6875cbf5d8c8bc645b178
rakshith@vantageagora.com <rakshith@vantageagora.com>
2020-02-04 11:43:19 +0530

    @Bug: 4862 - Completed the changes to the Attached module test cases and logs

--- api/v1/module/Attachment/test/Controller/AttachmentControllerTest.php
+++ api/v1/module/Attachment/test/Controller/AttachmentControllerTest.php
@@ -31,8 +31,15 @@ class AttachmentControllerTest extends ControllerTest
         FileUtils::createDirectory($tempFolder);
         copy(__DIR__ . "/../files/oxzionlogo.png", $tempFolder . "oxzionlogo.png");
         $data = array('type' => 'ANNOUNCEMENT', 'files' => array(array('extension' => 'png', 'uuid' => 'test', 'file_name' => 'oxzionlogo')));
-        $this->setJsonContent(json_encode($data));
-        $this->dispatch('/attachment', 'POST', null);
+
+        $_FILES['file'] = array();
+        $_FILES['file']['name'] = 'oxzionlogo.png';
+        $_FILES['file']['type'] = 'png';
+        $_FILES['file']['tmp_name'] = __DIR__ . '/../files/oxzionlogo.png';
+        $_FILES['file']['error'] = 0;
+        $_FILES['file']['size'] = 1007;
+
+        $this->dispatch('/attachment', 'POST', $data);
         $this->assertResponseStatusCode(201);
         $this->assertModuleName('Attachment');
         $this->assertControllerName(AttachmentController::class); // as specified in router's controller name alias
@@ -50,9 +57,10 @@ class AttachmentControllerTest extends ControllerTest
         $tempFolder = $config['UPLOAD_FOLDER'] . "organization/" . $this->testOrgId . "/announcements/";
         FileUtils::createDirectory($tempFolder);
         copy(__DIR__ . "/../files/oxzionlogo.png", $tempFolder . "oxzionlogo.png");
+
+        $_FILES = null; // Since this is a global variable, we need to set it to null
         $data = array('type' => 'ANNOUNCEMENT');
-        $this->setJsonContent(json_encode($data));
-        $this->dispatch('/attachment', 'POST', null);
+        $this->dispatch('/attachment', 'POST', $data);
         $this->assertResponseStatusCode(201);
         $this->assertModuleName('Attachment');
         $this->assertControllerName(AttachmentController::class); // as specified in router's controller name alias
@@ -60,6 +68,36 @@ class AttachmentControllerTest extends ControllerTest
         $this->assertMatchedRouteName('attachment');
         $this->assertResponseHeaderContains('content-type', 'application/json; charset=utf-8');
         $content = (array) json_decode($this->getResponse()->getContent(), true);
-        $this->assertEquals($content['status'], 'success');
+        // print_r($content);exit;
+        $this->assertEquals($content['status'], 'error');
+        $this->assertEquals($content['message'], 'File Not attached');
+    }
+
+    public function testAnnouncementWithoutFileTypeCreate()
+    {
+        $this->initAuthToken($this->adminUser);
+        $config = $this->getApplicationConfig();
+        $tempFolder = $config['UPLOAD_FOLDER'] . "organization/" . $this->testOrgId . "/announcements/";
+        FileUtils::createDirectory($tempFolder);
+        copy(__DIR__ . "/../files/oxzionlogo.png", $tempFolder . "oxzionlogo.png");
+
+        $_FILES['file'] = array();
+        $_FILES['file']['name'] = 'oxzionlogo.png';
+        $_FILES['file']['type'] = 'png';
+        $_FILES['file']['tmp_name'] = __DIR__ . '/../files/oxzionlogo.png';
+        $_FILES['file']['error'] = 0;
+        $_FILES['file']['size'] = 1007;
+
+        $data = null;
+        $this->dispatch('/attachment', 'POST', $data);
+        $this->assertResponseStatusCode(400);
+        $this->assertModuleName('Attachment');
+        $this->assertControllerName(AttachmentController::class); // as specified in router's controller name alias
+        $this->assertControllerClass('AttachmentController');
+        $this->assertMatchedRouteName('attachment');
+        $this->assertResponseHeaderContains('content-type', 'application/json; charset=utf-8');
+        $content = (array) json_decode($this->getResponse()->getContent(), true);
+        $this->assertEquals($content['status'], 'error');
+        $this->assertEquals($content['message'], 'File type not specified');
     }
 }
