2d25b75d4be61c80b2ad02ab3bc0c2ca0c34cde5
rakshith@vantageagora.com <rakshith@vantageagora.com>
2020-02-06 14:00:33 +0530

    @Bug: 4862 - Modified the test cases for AppDelegate

--- api/v1/module/App/src/Controller/AppDelegateController.php
+++ api/v1/module/App/src/Controller/AppDelegateController.php
@@ -3,14 +3,12 @@
 namespace App\Controller;
 
 use App\Model\App;
-use App\Model\AppTable;
 use App\Service\AppService;
-use Oxzion\ValidationException;
-use Oxzion\ServiceException;
-use Zend\Db\Adapter\AdapterInterface;
-use Oxzion\Controller\AbstractApiControllerHelper;
+use Exception;
 use Oxzion\AppDelegate\AppDelegateService;
+use Oxzion\Controller\AbstractApiControllerHelper;
 use Oxzion\Service\UserService;
+use Oxzion\ValidationException;
 
 class AppDelegateController extends AbstractApiControllerHelper
 {
@@ -19,14 +17,15 @@ class AppDelegateController extends AbstractApiControllerHelper
      */
     private $appDelegateService;
     private $userService;
-    
+
     /**
      * @ignore __construct
      */
-    public function __construct(AppDelegateService $appDelegateService,UserService $userService)
+    public function __construct(AppDelegateService $appDelegateService, UserService $userService)
     {
         $this->appDelegateService = $appDelegateService;
         $this->userService = $userService;
+        $this->log = $this->getLogger();
     }
     /**
      * App Register API
@@ -40,32 +39,47 @@ class AppDelegateController extends AbstractApiControllerHelper
         $data = $this->extractPostData();
         $appId = $this->params()->fromRoute()['appId'];
         $delegate = $this->params()->fromRoute()['delegate'];
-        $data = array_merge($data,$this->params()->fromQuery());
-        $this->appDelegateService->updateOrganizationContext($data);
-        $response = $this->appDelegateService->execute($appId, $delegate, $data);
-        if ($response == 1) {
-            return $this->getErrorResponse("Delegate not found", 404);
-        }else if($response == 2){
-            return $this->getErrorResponse("Error while executing the delegate", 400);
+        $data = array_merge($data, $this->params()->fromQuery());
+        $this->log->info(__CLASS__ . "-> \n Execute Delegate Start - " . print_r($data, true));
+        try {
+            $this->appDelegateService->updateOrganizationContext($data);
+            $response = $this->appDelegateService->execute($appId, $delegate, $data);
+            if ($response == 1) {
+                return $this->getErrorResponse("Delegate not found", 404);
+            } elseif ($response == 2) {
+                return $this->getErrorResponse("Error while executing the delegate", 400);
+            }
+        } catch (Exception $e) {
+            $this->logger->error($e->getMessage(), $e);
+            return $this->getErrorResponse($e->getMessage(), 400);
         }
+        $this->log->info(__CLASS__ . "-> \n End of Delegate");
         return $this->getSuccessResponseWithData($response, 200);
     }
-    
+
+    /**
+     * User List API
+     * @api
+     * @link /app/:appId/org/:orgId/userlist
+     * @method GET
+     * @param array $data - List of all the users for the organization
+     */
     public function userlistAction()
     {
         $params = array_merge($this->extractPostData(), $this->params()->fromRoute());
         try {
-            if(isset($params['appId']) && isset($params['orgId'])){
-                $users = $this->userService->getUsersList($params['appId'],$params  );
+            if (isset($params['appId']) && isset($params['orgId'])) {
+                $users = $this->userService->getUsersList($params['appId'], $params);
             } else {
                 $users = array();
             }
-        }catch (ValidationException $e) {
+        } catch (ValidationException $e) {
+            $this->log->error($e->getMessage(), $e);
             $response = ['errors' => $e->getErrors()];
-            return $this->getErrorResponse("Validation Errors",404, $response);
-        }catch (ServiceException $e) {
-            $response = ['errors' => $e->getMessageCode()];
-            return $this->getErrorResponse("App not found Errors",403, $response);
+            return $this->getErrorResponse("Validation Errors", 404, $response);
+        } catch (Exception $e) {
+            $this->log->error($e->getMessage(), $e);
+            return $this->getErrorResponse($e->getMessage(), 400);
         }
         return $this->getSuccessResponseWithData($users);
     }
