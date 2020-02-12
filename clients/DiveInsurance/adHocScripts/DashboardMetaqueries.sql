78ee1263d85cad5a29f0253a1affa4ca4ea0224b
Rajesh S <rajesha.s@gmail.com>
2020-02-12 12:06:27 +0530

    Added a note

--- clients/DiveInsurance/adHocScripts/DashboardMetaqueries.sql
+++ clients/DiveInsurance/adHocScripts/DashboardMetaqueries.sql
@@ -41,4 +41,5 @@ INSERT INTO ox_dashboard (uuid,name,ispublic,description,dashboard_type,created_
 ',1)
 ;
 
+--This is to be modified to properly point to the right end point
 update ox_datasource set configuration = '{"data": {"user": "elastic","password": "changeme","serveraddress": "13.52.224.89","port": "9200", "core":"oxzion", "type":"type", "scheme":"http"}}' where name LIKE 'OxzionElasticDs';
