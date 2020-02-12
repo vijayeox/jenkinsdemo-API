b7b053582f311be61b075a659c0006bcdc5b9f00
nehar <neha@myvamla.com>
2020-02-12 10:43:55 +0530

    Removed Database name in migration

--- api/v1/data/migrations/Version20200206073735.php
+++ api/v1/data/migrations/Version20200206073735.php
@@ -19,14 +19,14 @@ final class Version20200206073735 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        $this->addSql("ALTER TABLE `appBuilder`.`ox_app_entity` ADD COLUMN `assoc_id` INT NULL AFTER `date_modified`");
-        $this->addSql("ALTER TABLE `appBuilder`.`ox_file` ADD COLUMN `assoc_id` INT NULL AFTER `parent_id`");
+        $this->addSql("ALTER TABLE `ox_app_entity` ADD COLUMN `assoc_id` INT NULL AFTER `date_modified`");
+        $this->addSql("ALTER TABLE `ox_file` ADD COLUMN `assoc_id` INT NULL AFTER `parent_id`");
     }
 
     public function down(Schema $schema) : void
     {
-        $this->addSql("ALTER TABLE `appBuilder`.`ox_app_entity` DROP COLUMN `assoc_id`");
-        $this->addSql("ALTER TABLE `appBuilder`.`ox_file` DROP COLUMN `assoc_id`");
+        $this->addSql("ALTER TABLE `ox_app_entity` DROP COLUMN `assoc_id`");
+        $this->addSql("ALTER TABLE `ox_file` DROP COLUMN `assoc_id`");
 
     }
 }
