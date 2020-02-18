e5a9fb7d659ba9760645d9570fea246f2dac3167
rakshith@vantageagora.com <rakshith@vantageagora.com>
2020-02-18 15:34:24 +0530

    Modification to the Attachment related test cases

--- api/v1/module/Attachment/src/Module.php
+++ api/v1/module/Attachment/src/Module.php
@@ -2,14 +2,13 @@
 
 namespace Attachment;
 
+use Oxzion\Error\ErrorHandler;
 use Zend\Db\Adapter\AdapterInterface;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
-use Zend\View\Model\JsonModel;
-use Oxzion\Error\ErrorHandler;
 
 class Module implements ConfigProviderInterface
 {
