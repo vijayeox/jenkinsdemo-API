1c0c7364c0772e4b8e5ce90d1ab03627df945a9c
bharatg <bharatg@myvamla.com>
2020-03-30 19:45:14 +0530

    Quote Link going wrong

--- clients/DiveInsurance/data/delegate/DispatchProposalDocument.php
+++ clients/DiveInsurance/data/delegate/DispatchProposalDocument.php
@@ -27,23 +27,22 @@ class DispatchProposalDocument extends DispatchDocument {
         $fileData =array();
         $errorFile = array();
         foreach($data['documents'] as $doc){
-                    $file = $this->destination.$doc;
-                    if(file_exists($file)){
-                         array_push($fileData, $file);         
-                    }else{
-                        $this->logger->error("File Not Found".$file);
-                        array_push($errorFile,$file);
-                    }
+            $file = $this->destination.$doc;
+            if(file_exists($file)){
+                array_push($fileData, $file);         
+            } else {
+                $this->logger->error("File Not Found".$file);
+                array_push($errorFile,$file);
             }
-
-            if(count($errorFile) > 0){
-                $error = json_encode($errorFile);
-                $this->logger->error("Documents Not Found".$error);
-                throw new DelegateException('Documents Not Found','file.not.found',0,$errorFile);
-            }
-
+        }
+        if(count($errorFile) > 0){
+            $error = json_encode($errorFile);
+            $this->logger->error("Documents Not Found".$error);
+            throw new DelegateException('Documents Not Found','file.not.found',0,$errorFile);
+        }
         $data['document'] =$fileData;
         $data['subject'] = 'Proposal Document';
+        $data['url'] = $this->baseUrl. '?app=DiveInsurance&params={"type":"Form","activityInstanceId":"'.$data['activityInstanceId'].'","workflowInstanceId":"'.$data['workflowInstanceId'].'"}';
         $response = $this->dispatch($data);
         return $response;
     }
