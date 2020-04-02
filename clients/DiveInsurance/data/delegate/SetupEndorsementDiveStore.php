e07b1fccef808689bfcf6d7e3ee3f36fa14d2504
bharatg <bharatg@myvamla.com>
2020-04-02 15:48:53 +0530

    Endorsement Bug Fixes

--- clients/DiveInsurance/data/delegate/SetupEndorsementDiveStore.php
+++ clients/DiveInsurance/data/delegate/SetupEndorsementDiveStore.php
@@ -149,7 +149,7 @@ public function execute(array $data,Persistence $persistenceService)
         $endorsementLiabilityCoverageOption = array();
         $data['initiatedByCsr'] = false;
         $data['initiatedByUser'] = isset($data['initiatedByUser']) ? $data['initiatedByUser'] : false;
-        if($data['initiatedByUser'] == false){
+        if(($data['initiatedByUser'] == true && $data['initiatedByCsr'] == false) || ($data['initiatedByCsr'] == true && $data['initiatedByUser'] == false)){
             $data['update_date'] =  date("Y-m-d H:i:s");
             $data['previous_groupTaxPercentage'] = $data['groupTaxPercentage'];
             $data['previous_groupTaxAmount'] = ($data['groupTaxAmount']==0)?$data['groupTaxAmount']:0.00;
