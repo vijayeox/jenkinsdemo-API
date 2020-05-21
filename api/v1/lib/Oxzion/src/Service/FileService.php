f35d44c7a20548d2e335298202b6a5559d5cb68c
Prajwal <prajwal@usa.com>
2020-05-21 22:27:12 +0530

    Add true to json_decode

--- api/v1/lib/Oxzion/src/Service/FileService.php
+++ api/v1/lib/Oxzion/src/Service/FileService.php
@@ -703,7 +703,7 @@ class FileService extends AbstractService
                             $content = json_decode($file['data'], true);
                             if ($content) {
                                 if (isset($filterParams['columns'])) {
-                                    $filterParams['columns'] = json_decode($filterParams['columns']);
+                                    $filterParams['columns'] = json_decode($filterParams['columns'],true);
                                     foreach ($filterParams['columns'] as $column){
                                         $file[$column] = $content[$column];
                                     }                                    
