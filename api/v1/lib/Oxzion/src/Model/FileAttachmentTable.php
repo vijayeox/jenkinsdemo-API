41a28d000ce8c505458f1bdf17cede6fcb20b820
bharatg <bharatg@myvamla.com>
2020-05-31 03:07:52 +0530

    Add File Attachment Controller For storing Attachments into System

---
+++ api/v1/lib/Oxzion/src/Model/FileAttachmentTable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Oxzion\Model;
+
+use Oxzion\Db\ModelTable;
+use Oxzion\Model\Entity;
+use Zend\Db\TableGateway\TableGatewayInterface;
+
+class FileAttachmentTable extends ModelTable
+{
+    public function __construct(TableGatewayInterface $tableGateway)
+    {
+        parent::__construct($tableGateway);
+    }
+
+    public function save(Entity $data)
+    {
+        return $this->internalSave($data->toArray());
+    }
+}
