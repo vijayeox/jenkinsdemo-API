6bb8bfbb3960067a0bf574c3993f9ba3c3a6c3a7
Rajesh S <rajesha.s@gmail.com>
2020-06-16 16:35:51 +0530

    The return data was incorrect. Fixed it

--- api/v1/lib/Oxzion/src/Service/JobService.php
+++ api/v1/lib/Oxzion/src/Service/JobService.php
@@ -117,9 +117,10 @@ class JobService extends AbstractService
                 throw $e;
             }
         }
+        unset($response['Success']);
+        unset($response['Message']);
         $this->logger->info("Schedule JOB DATA in job service- " . print_r($jobData, true));
-        $this->logger->info("Return Schedule Job response job service- " . print_r($responseReturn, true));
-        return $responseReturn;
+        return $response;
     }
     
     public function cancelJob($jobName, $jobGroup, $appId)
