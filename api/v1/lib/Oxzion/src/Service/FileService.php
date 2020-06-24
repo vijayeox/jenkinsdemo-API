93461357369091b39705bdc026f5ba5bad08ec6e
nehar <neha@myvamla.com>
2020-06-24 11:59:21 +0530

    Missed in last commit

--- api/v1/lib/Oxzion/src/Service/FileService.php
+++ api/v1/lib/Oxzion/src/Service/FileService.php
@@ -409,7 +409,7 @@ class FileService extends AbstractService
                     $indexedField['date_modified'] = date('Y-m-d H:i:s'); 
                     $fieldvalue = isset($fieldData[$field['name']]) ? $fieldData[$field['name']] : null;
                     $indexedField = array_merge($indexedField,$this->generateFieldPayload($field['data_type'],$field,$indexedField,$fieldvalue,$entityId,$fileId,$fileArray));
-                    $indexedField['data'][$field['name']] = $indexedField[$field['name']]; 
+                    $indexedFields['data'][$field['name']] = $indexedField[$field['name']]; 
 
                     if(isset($indexedField['data'])){
                         $fieldvalueIndexed = json_encode($indexedField['data']);
