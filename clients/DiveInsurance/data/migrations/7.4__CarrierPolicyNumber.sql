7132b50ca3a3339aca279116562fac4fc9f5470f
nehar <neha@myvamla.com>
2020-06-26 17:25:43 +0530

    Guam PPK - Binder

--- clients/DiveInsurance/data/migrations/7.4__CarrierPolicyNumber.sql
+++ clients/DiveInsurance/data/migrations/7.4__CarrierPolicyNumber.sql
@@ -4,13 +4,15 @@ INSERT INTO `carrier_policy` (`product`,`carrier`,`policy_number`,`category`,`st
 
 UPDATE `carrier_policy` SET `policy_number` = 'PPK2148852' WHERE `product` in ('Individual Professional Liability','Group Professional Liability','Emergency First Response');
 
+
+UPDATE `carrier_policy` SET `policy_number` = 'Binder' WHERE `product` = 'Individual Professional Liability' AND `state` = 'Guam';
 DELETE FROM `carrier_policy` WHERE `year` = 2020;
 INSERT INTO `carrier_policy` (`product`,`carrier`,`policy_number`,`category`,`start_date`,`end_date`,`year`) VALUES ('Dive Store','Tokio Marine Specialty Insurance Company','PPK2148867','LIABILITY',"2020-06-30 00:00:00","2021-06-30 11:59:59",2020); 
 INSERT INTO `carrier_policy` (`product`,`carrier`,`policy_number`,`category`,`start_date`,`end_date`,`year`) VALUES ('Dive Store','Tokio Marine Specialty Insurance Company','PPK2148865','PROPERTY',"2020-06-30 00:00:00","2021-06-30 11:59:59",2020); 
 
 
 INSERT INTO `carrier_policy` (`product`,`carrier`,`policy_number`,`start_date`,`end_date`,`year`) VALUES ('Individual Professional Liability','Tokio Marine Specialty Insurance Company','PPK2148852',"2020-06-30 00:00:00","2021-06-30 11:59:59",2020); 
-INSERT INTO `carrier_policy` (`product`,`carrier`,`policy_number`,`state`,`start_date`,`end_date`,`year`) VALUES ('Individual Professional Liability', 'Tokio Marine Specialty Insurance Company', 'PPK2148852','Guam',"2020-06-30 00:00:00","2021-06-30 11:59:59",2020); 
+INSERT INTO `carrier_policy` (`product`,`carrier`,`policy_number`,`state`,`start_date`,`end_date`,`year`) VALUES ('Individual Professional Liability', 'Tokio Marine Specialty Insurance Company', 'Binder','Guam',"2020-06-30 00:00:00","2021-06-30 11:59:59",2020); 
 
 INSERT INTO `carrier_policy` (`product`,`carrier`,`policy_number`,`start_date`,`end_date`,`year`) VALUES ('Emergency First Response','Tokio Marine Specialty Insurance Company','PPK2148852',"2020-06-30 00:00:00","2021-06-30 11:59:59",2020); 
 INSERT INTO `carrier_policy` (`product`,`carrier`,`policy_number`,`start_date`,`end_date`,`year`) VALUES ('Group Professional Liability','Tokio Marine Specialty Insurance Company','PPK2148852',"2020-06-01 00:00:00","2021-06-30 11:59:59",2020); 
