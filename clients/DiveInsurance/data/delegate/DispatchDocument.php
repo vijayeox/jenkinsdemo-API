a24feb28b20c41c50b42828577e69b84d47a57fe
nehar <neha@myvamla.com>
2020-06-26 10:19:27 +0530

    Dispatch Documents to additional emails list

--- clients/DiveInsurance/data/delegate/DispatchDocument.php
+++ clients/DiveInsurance/data/delegate/DispatchDocument.php
@@ -17,6 +17,14 @@ abstract class DispatchDocument extends MailDelegate
         $mailOptions = array();
         $fileData = array();
         $mailOptions['to'] = $data['email'];
+        if(isset($data['additional_email'])){
+            $receiverEmailList = is_string($data['additional_email']) ? json_decode($data['additional_email'],true) : $data['additional_email'];
+            if(isset($receiverEmailList) && count($receiverEmailList) > 0){
+                array_unshift($receiverEmailList, $data['email']);
+                $mailOptions['to'] = $receiverEmailList;
+            }
+        }
+              
         $mailOptions['subject'] = $data['subject'];
         if (isset($data['document'])) {
             $mailOptions['attachments'] = $data['document'];
