b0b88bf8913500131554f74b86fc0337c0858bef
purvi <purvi@myvamla.com>
2020-08-25 16:19:26 +0530

    Hidden field and mapping changes.

--- clients/ArrowHead/view/apps/ArrowHead/Hidden Field/rpsCyberPDFData.js
+++ clients/ArrowHead/view/apps/ArrowHead/Hidden Field/rpsCyberPDFData.js
@@ -33,22 +33,22 @@ if (data.workbooksToBeGenerated.rpsCyber == true) {
     if (data.ofRecordsContaining < 100000) {
       checkofRecordsContaining = "1-100,000";
     } else if (
-      data.ofRecordsContaining > 100000 &&
+      data.ofRecordsContaining >= 100000 &&
       data.ofRecordsContaining < 250000
     ) {
       checkofRecordsContaining = "100,000-250,000";
     } else if (
-      data.ofRecordsContaining > 250000 &&
+      data.ofRecordsContaining >= 250000 &&
       data.ofRecordsContaining < 500000
     ) {
       checkofRecordsContaining = "250,000-500,000";
     } else if (
-      data.ofRecordsContaining > 500000 &&
+      data.ofRecordsContaining >= 500000 &&
       data.ofRecordsContaining < 750000
     ) {
       checkofRecordsContaining = "500,000-750,000";
     } else if (
-      data.ofRecordsContaining > 750000 &&
+      data.ofRecordsContaining >= 750000 &&
       data.ofRecordsContaining < 1000000
     ) {
       checkofRecordsContaining = "750,000-1,000,000";
