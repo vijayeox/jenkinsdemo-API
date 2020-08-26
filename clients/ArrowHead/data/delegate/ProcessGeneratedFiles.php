b37f5700ed331bfc170f68a0a91b1dcf8a3a78d6
Prajwal K <prajwals@eoxvantage.in>
2020-08-27 03:33:21 +0530

    Send policy docs in final email

--- clients/ArrowHead/data/delegate/ProcessGeneratedFiles.php
+++ clients/ArrowHead/data/delegate/ProcessGeneratedFiles.php
@@ -97,14 +97,26 @@ class ProcessGeneratedFiles extends MailDelegate
         $selectQuery = "Select value FROM applicationConfig WHERE type ='arrowHeadInboxMail'";
         $submissionEmail = ($persistenceService->selectQuery($selectQuery))->current()["value"];
 
+        $emailAttachments = [];
+        foreach ($this->checkJSON($data['documents']) as $doc) {
+            if ($doc['originalName'] !== "excelMapperInput.json") {
+                if(isset($doc["fullPath"])){
+                    array_push($emailAttachments, $doc['fullPath'] );
+                } else {
+                    array_push($emailAttachments, $doc['path'] );
+                }
+            }
+        }
+
         $mailOptions = array();
         $mailOptions['to'] = $submissionEmail;
         $mailOptions['subject'] = "New business – " . $data['namedInsured'] . " - " . $this->formatDate($data['effectiveDate']) . " - " . $data['producername'];
+        $mailOptions['attachments'] = $emailAttachments;
         $this->logger->info("Arrowhead Policy Mail " . print_r($mailOptions, true));
         $data['orgUuid'] = "34bf01ab-79ca-42df-8284-965d8dbf290e";
         // $data['orgUuid'] = isset($data['orgId']) ? $data['orgId'] : AuthContext::get(AuthConstants::ORG_UUID);
         $response = $this->sendMail($data, "finalSubmissionMail", $mailOptions);
-        $this->logger->info("Mail Response" . $response);
+        $this->logger->info("Mail has " . $response ? "been sent." : "not been sent.");
         return $response;
     }
 
@@ -115,5 +127,13 @@ class ProcessGeneratedFiles extends MailDelegate
             "m-d-Y",
             strtotime($date)
         );
+    }
+
+    private function checkJSON($data)
+    {
+        if (!is_array($data)) {
+            $data = json_decode($data, true);
+        }
+        return $data;
     }
 }
