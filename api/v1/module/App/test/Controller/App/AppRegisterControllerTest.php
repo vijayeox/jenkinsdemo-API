8e45f034afdc646a9608f99145105a84453ce090
bharatg <bharatg@myvamla.com>
2020-09-24 03:18:51 +0530

    [REFACTOR] Uncomment Test Cases Execution

--- api/v1/module/App/test/Controller/App/AppRegisterControllerTest.php
+++ api/v1/module/App/test/Controller/App/AppRegisterControllerTest.php
@@ -49,36 +49,36 @@ class AppRegisterContollerTest extends ControllerTest
         $result = $statement->execute();
     }
 
-    // public function testAppRegister()
-    // {
-    //     $this->initAuthToken($this->adminUser);
-    //     $data = ['applist' => json_encode([["name" => "CRM", "category" => "organization", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Calculator", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Calendar", "category" => "collaboration", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Chat", "category" => "collaboration", "options" => ["autostart" => "true", "hidden" => "true"]], ["name" => "FileManager", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Mail", "category" => "collaboration", "options" => ["autostart" => "true", "hidden" => "true"]], ["name" => "MailAdmin", "category" => "utilities", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "MyTodo", "category" => "null", "options" => ["autostart" => "false", "hidden" => "true"]], ["name" => "Textpad", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]]])];
-    //     $this->setJsonContent(json_encode($data));
-    //     $this->dispatch('/app/register', 'POST', $data);
-    //     $this->assertResponseStatusCode(200);
-    //     $this->assertModuleName('App');
-    //     $this->assertControllerName(AppRegisterController::class); // as specified in router's controller name alias
-    //     $this->assertControllerClass('AppRegisterController');
-    //     $this->assertMatchedRouteName('appregister');
-    //     $this->assertResponseHeaderContains('content-type', 'application/json; charset=utf-8');
-    //     $content = (array) json_decode($this->getResponse()->getContent(), true);
-    //     $this->assertEquals($content['status'], 'success');
-    // }
+    public function testAppRegister()
+    {
+        $this->initAuthToken($this->adminUser);
+        $data = ['applist' => json_encode([["name" => "CRM", "category" => "organization", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Calculator", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Calendar", "category" => "collaboration", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Chat", "category" => "collaboration", "options" => ["autostart" => "true", "hidden" => "true"]], ["name" => "FileManager", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Mail", "category" => "collaboration", "options" => ["autostart" => "true", "hidden" => "true"]], ["name" => "MailAdmin", "category" => "utilities", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "MyTodo", "category" => "null", "options" => ["autostart" => "false", "hidden" => "true"]], ["name" => "Textpad", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]]])];
+        $this->setJsonContent(json_encode($data));
+        $this->dispatch('/app/register', 'POST', $data);
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('App');
+        $this->assertControllerName(AppRegisterController::class); // as specified in router's controller name alias
+        $this->assertControllerClass('AppRegisterController');
+        $this->assertMatchedRouteName('appregister');
+        $this->assertResponseHeaderContains('content-type', 'application/json; charset=utf-8');
+        $content = (array) json_decode($this->getResponse()->getContent(), true);
+        $this->assertEquals($content['status'], 'success');
+    }
 
-    // public function testAppRegisterInvaliddata()
-    // {
-    //     $this->initAuthToken($this->adminUser);
-    //     $data = ['applist' => json_encode([["name" => "", "category" => "organization", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Calculator", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Calendar", "category" => "collaboration", "" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Chat", "category" => "collaboration", "options" => ["autostart" => "true", "hidden" => "true"]], ["name" => "FileManager", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Mail", "category" => "collaboration", "options" => ["autostart" => "true", "hidden" => "true"]], ["name" => "MailAdmin", "category" => "utilities", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "MyTodo", "category" => "null", "options" => ["autostart" => "false", "hidden" => "true"]], ["name" => "Textpad", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]]])];
-    //     $this->setJsonContent(json_encode($data));
-    //     $this->dispatch('/app/register', 'POST', $data);
-    //     $this->assertResponseStatusCode(404);
-    //     $this->assertModuleName('App');
-    //     $this->assertControllerName(AppRegisterController::class); // as specified in router's controller name alias
-    //     $this->assertControllerClass('AppRegisterController');
-    //     $this->assertMatchedRouteName('appregister');
-    //     $this->assertResponseHeaderContains('content-type', 'application/json; charset=utf-8');
-    //     $content = (array) json_decode($this->getResponse()->getContent(), true);
-    //     $this->assertEquals($content['status'], 'error');
-    // }
+    public function testAppRegisterInvaliddata()
+    {
+        $this->initAuthToken($this->adminUser);
+        $data = ['applist' => json_encode([["name" => "", "category" => "organization", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Calculator", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Calendar", "category" => "collaboration", "" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Chat", "category" => "collaboration", "options" => ["autostart" => "true", "hidden" => "true"]], ["name" => "FileManager", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "Mail", "category" => "collaboration", "options" => ["autostart" => "true", "hidden" => "true"]], ["name" => "MailAdmin", "category" => "utilities", "options" => ["autostart" => "false", "hidden" => "false"]], ["name" => "MyTodo", "category" => "null", "options" => ["autostart" => "false", "hidden" => "true"]], ["name" => "Textpad", "category" => "office", "options" => ["autostart" => "false", "hidden" => "false"]]])];
+        $this->setJsonContent(json_encode($data));
+        $this->dispatch('/app/register', 'POST', $data);
+        $this->assertResponseStatusCode(404);
+        $this->assertModuleName('App');
+        $this->assertControllerName(AppRegisterController::class); // as specified in router's controller name alias
+        $this->assertControllerClass('AppRegisterController');
+        $this->assertMatchedRouteName('appregister');
+        $this->assertResponseHeaderContains('content-type', 'application/json; charset=utf-8');
+        $content = (array) json_decode($this->getResponse()->getContent(), true);
+        $this->assertEquals($content['status'], 'error');
+    }
 
 }
