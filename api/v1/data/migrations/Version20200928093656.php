6abb4e5054c27b156801f8f274ca01730ee08b0e
Rajesh S <rajesha.s@gmail.com>
2020-09-28 16:46:54 +0530

    Added main organization id to the organization table

--- api/v1/data/migrations/Version20200928093656.php
+++ api/v1/data/migrations/Version20200928093656.php
@@ -21,7 +21,20 @@ final class Version20200928093656 extends AbstractMigration
     {
         $this->addSql("ALTER TABLE ox_organization ADD COLUMN parent_id INT(32)");
         $this->addSql("ALTER TABLE ox_organization ADD CONSTRAINT FOREIGN KEY (`parent_id`) REFERENCES ox_organization(`id`)");
-        
+        $this->addSql("ALTER TABLE ox_organization ADD COLUMN main_organization_id INT(32)");
+        $this->addSql("UPDATE ox_organization set main_organization_id = id");
+        $this->addSql("ALTER TABLE ox_organization change main_organization_id main_organization_id INT(32) ");
+        $this->addSql("ALTER TABLE ox_organization ADD CONSTRAINT FOREIGN KEY (`main_organization_id`) REFERENCES ox_organization(`id`)");
+        $this->addSql("CREATE TRIGGER `ox_organization_insert` BEFORE INSERT ON `ox_organization`
+            FOR EACH ROW
+            BEGIN
+              IF NEW.main_organization_id IS NULL THEN
+                SET NEW.main_organization_id := ( SELECT AUTO_INCREMENT 
+                       FROM INFORMATION_SCHEMA.TABLES
+                      WHERE TABLE_NAME = 'ox_organization'
+                        AND TABLE_SCHEMA = DATABASE() );
+              END IF;
+            END;");
 
     }
 
