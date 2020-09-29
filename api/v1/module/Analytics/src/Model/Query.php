e56bf5eebe69eabc5f50b55b2661f18f10a3d41b
Rakshith <rakshith@vantageagora.com>
2020-09-29 13:56:46 +0530

    [BUG]: 14126 - Modified the query service to validate the string

--- api/v1/module/Analytics/src/Model/Query.php
+++ api/v1/module/Analytics/src/Model/Query.php
@@ -2,25 +2,27 @@
 
 namespace Analytics\Model;
 
-use Oxzion\Type;
 use Oxzion\Model\Entity;
+use Oxzion\Type;
 
-class Query extends Entity {
+class Query extends Entity
+{
     protected static $MODEL = [
-        'id' =>             ['type' => Type::INTEGER,   'readonly' => TRUE ,    'required' => FALSE],
-        'uuid' =>           ['type' => Type::UUID,      'readonly' => TRUE ,    'required' => FALSE],
-        'name' =>           ['type' => Type::STRING,    'readonly' => FALSE ,   'required' => TRUE],
-        'datasource_id' =>  ['type' => Type::INTEGER,   'readonly' => FALSE ,   'required' => TRUE],
-        'configuration' =>  ['type' => Type::STRING,    'readonly' => FALSE ,   'required' => TRUE],
-        'ispublic' =>       ['type' => Type::BOOLEAN,   'readonly' => FALSE ,   'required' => FALSE, 'value' => FALSE],
-        'created_by' =>     ['type' => Type::INTEGER,   'readonly' => TRUE ,    'required' => FALSE],
-        'date_created' =>   ['type' => Type::TIMESTAMP, 'readonly' => TRUE ,    'required' => FALSE],
-        'org_id' =>         ['type' => Type::INTEGER,   'readonly' => TRUE ,    'required' => FALSE],
-        'isdeleted' =>      ['type' => Type::BOOLEAN,   'readonly' => FALSE ,   'required' => FALSE, 'value' => FALSE],
-        'version' =>        ['type' => Type::INTEGER,   'readonly' => FALSE,    'required' => FALSE]
+        'id' => ['type' => Type::INTEGER, 'readonly' => true, 'required' => false],
+        'uuid' => ['type' => Type::UUID, 'readonly' => true, 'required' => false],
+        'name' => ['type' => Type::STRING, 'readonly' => false, 'required' => true],
+        'datasource_id' => ['type' => Type::INTEGER, 'readonly' => false, 'required' => true],
+        'configuration' => ['type' => Type::STRING, 'readonly' => false, 'required' => true],
+        'ispublic' => ['type' => Type::BOOLEAN, 'readonly' => false, 'required' => false, 'value' => false],
+        'created_by' => ['type' => Type::INTEGER, 'readonly' => true, 'required' => false],
+        'date_created' => ['type' => Type::TIMESTAMP, 'readonly' => true, 'required' => false],
+        'org_id' => ['type' => Type::INTEGER, 'readonly' => true, 'required' => false],
+        'isdeleted' => ['type' => Type::BOOLEAN, 'readonly' => false, 'required' => false, 'value' => false],
+        'version' => ['type' => Type::INTEGER, 'readonly' => false, 'required' => false],
     ];
 
-    protected function &getModel() {
+    protected function &getModel()
+    {
         return self::$MODEL;
     }
 }
