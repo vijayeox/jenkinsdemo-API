d2deab217b69c4c536ddee5674550c53efbdb5b0
Rajesh S <rajesha.s@gmail.com>
2020-09-29 17:27:56 +0530

    Changes to migration to have the org heirarcy in a separate table

--- api/v1/data/migrations/Version20200928093656.php
+++ api/v1/data/migrations/Version20200928093656.php
@@ -21,21 +21,14 @@ final class Version20200928093656 extends AbstractMigration
     {
         $this->addSql("ALTER TABLE ox_organization ADD COLUMN parent_id INT(32)");
         $this->addSql("ALTER TABLE ox_organization ADD CONSTRAINT FOREIGN KEY (`parent_id`) REFERENCES ox_organization(`id`)");
-        $this->addSql("ALTER TABLE ox_organization ADD COLUMN main_organization_id INT(32)");
-        $this->addSql("UPDATE ox_organization set main_organization_id = id");
-        $this->addSql("ALTER TABLE ox_organization change main_organization_id main_organization_id INT(32) ");
-        $this->addSql("ALTER TABLE ox_organization ADD CONSTRAINT FOREIGN KEY (`main_organization_id`) REFERENCES ox_organization(`id`)");
-        $this->addSql("CREATE TRIGGER `ox_organization_insert` BEFORE INSERT ON `ox_organization`
-            FOR EACH ROW
-            BEGIN
-              IF NEW.main_organization_id IS NULL THEN
-                SET NEW.main_organization_id := ( SELECT AUTO_INCREMENT 
-                       FROM INFORMATION_SCHEMA.TABLES
-                      WHERE TABLE_NAME = 'ox_organization'
-                        AND TABLE_SCHEMA = DATABASE() );
-              END IF;
-            END;");
-
+        $this->addSql("CREATE TABLE `ox_org_heirarchy` (
+            `main_org_id` INT(32) NOT NULL,
+            `parent_id` INT(32) NOT NULL,
+            `child_id` int(32) NOT NULL,
+            FOREIGN KEY (`main_org_id`) REFERENCES ox_organization(`id`),
+            FOREIGN KEY (`parent_id`) REFERENCES ox_organization(`id`),
+            FOREIGN KEY (`child_id`) REFERENCES ox_organization(`id`)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8");
     }
 
     public function down(Schema $schema) : void
