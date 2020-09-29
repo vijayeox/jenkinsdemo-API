37a616a1fe2338a18c63ed3f49bea5f75ff0dbd9
Rakshith <rakshith@vantageagora.com>
2020-09-29 13:56:46 +0530

    [BUG]: 14126 - Modified the query service to validate the string

--- api/v1/module/Analytics/src/Model/QueryTable.php
+++ api/v1/module/Analytics/src/Model/QueryTable.php
@@ -4,10 +4,11 @@ namespace Analytics\Model;
 
 use Oxzion\Db\ModelTable;
 use Zend\Db\TableGateway\TableGatewayInterface;
-use Oxzion\Model\Entity;
 
-class QueryTable extends ModelTable {
-    public function __construct(TableGatewayInterface $tableGateway) {
+class QueryTable extends ModelTable
+{
+    public function __construct(TableGatewayInterface $tableGateway)
+    {
         parent::__construct($tableGateway);
     }
 }
