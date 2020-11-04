030204ffa15aa0b6cba960971fde34241874c5d8
mkenia <mkenia@hotmail.com>
2020-11-04 16:36:26 -0500

    [BUG] 15336 - Fixed the PostGres OI issue

--- api/v1/lib/Oxzion/src/Analytics/Relational/AnalyticsEngineMySQLImpl.php
+++ api/v1/lib/Oxzion/src/Analytics/Relational/AnalyticsEngineMySQLImpl.php
@@ -2,7 +2,7 @@
 namespace Oxzion\Analytics\Relational;
 
 use Oxzion\Analytics\Relational\AnalyticsEngineRelational;
-
+use Zend\Db\Adapter\Adapter;
 
 class AnalyticsEngineMySQLImpl extends AnalyticsEngineRelational {
 
@@ -10,7 +10,16 @@ class AnalyticsEngineMySQLImpl extends AnalyticsEngineRelational {
 		parent::__construct($appDBAdapter,$appConfig);
     }
 
-    
+    public function setConfig($config){
+      $dbConfig['driver'] = 'Pdo';
+      $dbConfig['database'] = $config['database'];
+      $dbConfig['host'] = $config['host'];
+      $dbConfig['username'] = $config['username'];
+      $dbConfig['password'] = $config['password'];
+      $dbConfig['dsn'] = 'mysql:dbname=' . $config['database'] . ';host=' . $config['host'] . ';charset=utf8;username=' . $config["username"] . ';password=' . $config["password"] . '';
+      $this->dbConfig = $dbConfig;
+      $this->dbAdapter = new Adapter($dbConfig);  
+      }
 
 
 
