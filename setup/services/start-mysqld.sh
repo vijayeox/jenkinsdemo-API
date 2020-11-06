fcd20fd13db9b66e177053ed17da15e7dabc5cba
Rajesh S <rajesha.s@gmail.com>
2020-11-06 17:08:34 +0530

    minor modifications

--- setup/services/start-mysqld.sh
+++ setup/services/start-mysqld.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-exec mysqld_safe
+echo "=> Checking MySQL ..."
+RET=$(pgrep mysql | wc -l)
+echo $RET
+if [ $RET -ne 0 ]; then
+    echo "=> Starting MySQL ..."
+    if [ -e /var/run/mysqld/mysqld.sock ];then
+        rm /var/run/mysqld/mysqld.sock
+    fi
+
+    /usr/bin/mysqld_safe > /dev/null 2>&1 &
+
+    RET=0
+    while [[ RET -ne 0 ]]; do
+        echo "=> Waiting for confirmation of MySQL service startup"
+        sleep 5
+        RET=$(pgrep mysql | wc -l)
+    done
+fi
+echo "=> MySQL Running ..."
