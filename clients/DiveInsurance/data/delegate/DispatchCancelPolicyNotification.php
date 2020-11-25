e25fbab9945c4a331c06b7565524087df6530fe0
nehar <neha@myvamla.com>
2020-11-25 10:42:26 +0530

    [REQ] #15309:  After Cancellation all the documents should show up along with cancellation document.

--- clients/DiveInsurance/data/delegate/DispatchCancelPolicyNotification.php
+++ clients/DiveInsurance/data/delegate/DispatchCancelPolicyNotification.php
@@ -1,62 +1,64 @@
 <?php
-use Oxzion\AppDelegate\MailDelegate;
+
 use Oxzion\Db\Persistence\Persistence;
-use Oxzion\Messaging\MessageProducer;
-use Oxzion\Encryption\Crypto;
 use Oxzion\AppDelegate\FileTrait;
-require_once __DIR__."/DispatchDocument.php";
+use Oxzion\DelegateException;
+
+require_once __DIR__ . "/DispatchDocument.php";
 
 
-class DispatchCancelPolicyNotification extends DispatchDocument {
+class DispatchCancelPolicyNotification extends DispatchDocument
+{
     use FileTrait;
     public $template;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->template = array(
             'Individual Professional Liability' => 'CancelPolicyMailTemplate',
             'Emergency First Response' => 'CancelPolicyMailTemplate',
             'Dive Boat' => 'CancelPolicyMailTemplate',
             'Group Professional Liability' => 'CancelPolicyMailTemplate',
             'NotApproved' => 'CancelPolicyNotApprovedMailTemplate',
-            'Dive Store' => 'CancelPolicyMailTemplate');
+            'Dive Store' => 'CancelPolicyMailTemplate'
+        );
         parent::__construct();
     }
 
-    public function execute(array $data,Persistence $persistenceService)
+    public function execute(array $data, Persistence $persistenceService)
     {
         $this->logger->info("Dispatch Cancel Policy Notification");
-        $fileData = $this->getFile($data['fileId'],false,$data['orgId']);
-        $data = array_merge($data,$fileData['data']);
-        if($data['cancellationStatus'] =="approved")
-        {
+        $fileData = $this->getFile($data['fileId'], false, $data['orgId']);
+        $data = array_merge($data, $fileData['data']);
+        if ($data['cancellationStatus'] == "approved") {
             $this->logger->info("Dispatch Cancel Policy Notification -- approved");
             $data['template'] = $this->template[$data['product']];
-            if(isset($data['documents']) && is_string($data['documents'])){
-                $data['documents'] = json_decode($data['documents'],true);
+            if (isset($data['documents']) && is_string($data['documents'])) {
+                $data['documents'] = json_decode($data['documents'], true);
             }
-            $this->logger->info("ARRAY DOCUMENT --- ".json_encode($data['documents']));
+            $this->logger->info("ARRAY DOCUMENT --- " . json_encode($data['documents']));
             $this->logger->info("REQUIRED DOCUMENT --- cancel_doc");
             $fileData = array();
-            if(isset($data['documents']['cancel_doc'])){
-                $file = $this->destination.$data['documents']['cancel_doc'];
-                if(file_exists($file)){
-                     array_push($fileData, $file);
-                }else{
-                    $this->logger->error("Cancellation Document Not Found - ".$file);
-                    throw new DelegateException('Cancellation Document Not Found','file.not.found',0,array($file));
+            if (isset($data['documents']['cancel_doc'])) {
+                $file = $this->destination . $data['documents']['cancel_doc'][0];
+                if (file_exists($file)) {
+                    array_push($fileData, $file);
+                } else {
+                    $this->logger->error("Cancellation Document Not Found - " . $file);
+                    throw new DelegateException('Cancellation Document Not Found', 'file.not.found', 0, array($file));
                 }
-            }else{
-                $this->logger->error("Cancellation Document Not Found".$error);
-                throw new DelegateException('Cancellation Document Not Found','file.not.found');
+            } else {
+                $this->logger->error("Cancellation Document Not Found");
+                throw new DelegateException('Cancellation Document Not Found', 'file.not.found');
             }
             $mailData = array();
             $mailData = $data;
             $mailData['email'] = $data['email'];
-            if(isset($data['padi'])){
-                $mailData['subject'] = 'PADI Endorsed Insurance Cancellation – '.$data['padi'];
+            if (isset($data['padi'])) {
+                $mailData['subject'] = 'PADI Endorsed Insurance Cancellation – ' . $data['padi'];
             } else {
-                if(isset($data['business_padi'])){
-                    $mailData['subject'] = 'PADI Endorsed Insurance Cancellation – '.$data['business_padi'];
+                if (isset($data['business_padi'])) {
+                    $mailData['subject'] = 'PADI Endorsed Insurance Cancellation – ' . $data['business_padi'];
                 }
             }
             $mailData['template'] = $data['template'];
@@ -64,17 +66,18 @@ class DispatchCancelPolicyNotification extends DispatchDocument {
             $response = $this->dispatch($mailData);
             $data['autoRenewalJob'] = '';
             unset($data['confirmReinstatePolicy']);
-            return $data;
-        }
-        else{
+        } else {
             $this->logger->info("Dispatch Cancel Policy Notification -- not approved");
             $data['template'] = $this->template['NotApproved'];
             $data['subject'] = 'Request for cancellation of policy';
             $response = $this->dispatch($data);
             $data['policyStatus'] = "In Force";
             unset($data['confirmReinstatePolicy']);
-            return $data;
         }
+        if (isset($data['reinstateDocuments'])) {
+            $data['reinstateDocuments'] = is_string($data['reinstateDocuments']) ? json_decode($data['reinstateDocuments'], true) : $data['reinstateDocuments'];
+            $data['documents'] = array_merge($data['reinstateDocuments'], $data['documents']);
+        }
+        return $data;
     }
 }
-?>
