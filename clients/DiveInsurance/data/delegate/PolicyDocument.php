bb88b0e3101484122448a7c065a8222d8a416a41
brian <brianmp@eoxvantage.in>
2020-12-04 16:12:04 +0530

    [BUG]16132 - fix for undefined proposal count

--- clients/DiveInsurance/data/delegate/PolicyDocument.php
+++ clients/DiveInsurance/data/delegate/PolicyDocument.php
@@ -991,6 +991,7 @@ class PolicyDocument extends AbstractDocumentAppDelegate
             $reinstateDate = date_format(date_create($data['reinstateDate']), 'Md');
             $docDest = $dest['absolutePath'] . $template . '_' . $reinstateDate . '.pdf';
         }else if (($this->type == 'quote' || $this->type == 'endorsementQuote') && $data['product'] != 'Dive Boat'){
+            $data['proposalCount']=isset($data['proposalCount'])?$data['proposalCount']:1;
             $docDest = $dest['absolutePath'] . $template . '_' . $data['proposalCount'] . '.pdf';
         }else{
             $docDest = $dest['absolutePath'] . $template . '.pdf';
