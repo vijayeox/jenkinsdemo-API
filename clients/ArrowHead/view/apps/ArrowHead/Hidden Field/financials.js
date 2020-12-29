e411cf9b89eb868aa81f75165c35f038aeaa7045
Prajwal K <prajwals@eoxvantage.in>
2020-12-29 17:42:29 +0530

    [CR] 16694 - Annualized Grid Calculation logic

--- clients/ArrowHead/view/apps/ArrowHead/Hidden Field/financials.js
+++ clients/ArrowHead/view/apps/ArrowHead/Hidden Field/financials.js
@@ -17,23 +17,35 @@ var lastBuilding = getLastBuilding(data, currentLocation);
 console.log(lastBuilding);
 show = lastBuilding == parseInt(row.locationBuildingNum.split("")[2]);
 
-
 // ########################
 // Calculate Button
 // ########################
 
+console.log("click");
+
 var financeFieldList = [
   {
     key: "financialsYTDSales",
-    children: [
-      "bodyShop",
-      "fAndI",
-      "newAutos",
-      "parts",
-      "service",
-      "usedAutos",
-    ],
   },
+  {
+    key: "financialsYTDGrossProfits",
+  },
+  {
+    key: "financialsYTDSalesAnnualized",
+  },
+  {
+    key: "financialsYTDGrossProfitsAnnualized",
+  },
+];
+
+var childFields = [
+  "bodyShop",
+  "fAndI",
+  "newAutos",
+  "parts",
+  "service",
+  "usedAutos",
+  "rentalLeasing",
 ];
 
 var sumObjectsByKey = (...objs) => {
@@ -45,20 +57,35 @@ var sumObjectsByKey = (...objs) => {
   }, {});
 };
 
+var monthVariable =
+  data.financialStatementMonths > 0 ? 12 / data.financialStatementMonths : 1;
+
 financeFieldList.map((field) => {
-  var cloneItem = [...data[field.key]];
+  var cloneItem = [
+    ...data[
+      field.key.includes("Annual") ? field.key.split("Annual")[0] : field.key
+    ],
+  ];
   var accountTotal = {};
   var locationTotal = 0;
   var fieldTotal = {};
-  cloneItem = cloneItem.map((building, buildingIndex) => {
-    building.total = field.children
+
+  cloneItem = cloneItem.map((building) => {
+    field.key.includes("Annual")
+      ? childFields.map((i) => {
+          building[i] > 0 ? (building[i] = building[i] * monthVariable) : null;
+        })
+      : null;
+    building.total = childFields
       .map((i) => (building[i] > 0 ? building[i] : 0))
       .reduce((a, b) => a + b, 0);
     locationTotal += building.total;
+
     fieldTotal = sumObjectsByKey(fieldTotal, building);
+
     if (building.lastBuilding) {
       var cleanfieldTotal = {};
-      field.children.map((i) => {
+      childFields.map((i) => {
         cleanfieldTotal["total_" + i] = fieldTotal[i] ? fieldTotal[i] : 0;
         accountTotal[i] =
           (accountTotal[i] ? accountTotal[i] : 0) +
@@ -71,8 +98,23 @@ financeFieldList.map((field) => {
       locationTotal = 0;
       fieldTotal = {};
     }
+
     return building;
   });
+
   form.getComponent(field.key).setValue(cloneItem);
   form.getComponent(field.key + "Total").setValue(accountTotal);
+
+  if (field.key == "financialsYTDSales") {
+    cloneItem.map((item, index) => {
+      if (data.buildings[index].coinsuranceamount) {
+        var buildingCoinsurance = parseFloat(
+          data.buildings[index].coinsuranceamount
+        );
+        if (buildingCoinsurance > 0) {
+          
+        }
+      }
+    });
+  }
 });
