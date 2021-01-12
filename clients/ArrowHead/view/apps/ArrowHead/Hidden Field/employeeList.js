201e666fa07fb6cb7ddc7573d25c1f45809bad87
Prajwal K <prajwals@eoxvantage.in>
2021-01-12 18:50:52 +0530

    [CR] 14969 - Employee List component

---
+++ clients/ArrowHead/view/apps/ArrowHead/Hidden Field/employeeList.js
@@ -0,0 +1,61 @@
+// ########################
+// Calculate Button
+// ########################
+
+console.log("click2");
+
+var childFields = [
+  "fTEmployeesFurnishedAnAuto",
+  "pTEmployeesFurnishedAnAuto",
+  "fTEmployeesWhoAreNotFurnished",
+  "pTEmployeesWhoAreNotFurnished",
+  "fTAllOtherEmployees",
+  "pTAllOtherEmployees",
+  "nonEmployeesUnderTheAge",
+  "nonEmployeesYearsOldorolder",
+  "contractDriversNonEmployees",
+];
+
+var sumObjectsByKey = (...objs) => {
+  return objs.reduce((a, b) => {
+    for (let k in b) {
+      if (b.hasOwnProperty(k)) a[k] = (a[k] || 0) + b[k];
+    }
+    return a;
+  }, {});
+};
+
+var cloneItem = [...data["employeeList"]];
+var accountTotal = {};
+var locationTotal = 0;
+var fieldTotal = {};
+
+cloneItem = cloneItem.map((building) => {
+  building.total = childFields
+    .map((i) => (building[i] > 0 ? building[i] : 0))
+    .reduce((a, b) => a + b, 0);
+  locationTotal += building.total;
+
+  fieldTotal = sumObjectsByKey(fieldTotal, building);
+
+  if (building.lastBuilding) {
+    var cleanfieldTotal = {};
+    childFields.map((i) => {
+      cleanfieldTotal["total_" + i] = fieldTotal[i] ? fieldTotal[i] : 0;
+      accountTotal[i] =
+        (accountTotal[i] ? accountTotal[i] : 0) +
+        (fieldTotal[i] ? fieldTotal[i] : 0);
+    });
+    building.total_total = locationTotal;
+    building = { ...building, ...cleanfieldTotal };
+    accountTotal.total =
+      (accountTotal.total ? accountTotal.total : 0) + locationTotal;
+    locationTotal = 0;
+    fieldTotal = {};
+  }
+
+  return building;
+});
+
+form.getComponent("employeeList").setValue(cloneItem);
+form.getComponent("employeeList" + "Total").setValue(accountTotal);
