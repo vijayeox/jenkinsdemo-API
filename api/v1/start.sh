31ab05d57e9eae2a053c91feac85b58fc1e85b06
Karan Agarwal <karan@myvamla.com>
2021-03-04 18:32:18 +0530

    [REFACTOR] Simplify docker startup | just run ./start.sh on api or view folder.

--- api/v1/start.sh
+++ api/v1/start.sh
@@ -4,3 +4,16 @@ sed -ri -e "s/^HOST=.*/HOST=$IP/" \
 	.env
 
 docker-compose up -d --build
+
+getopts ":yn" yn
+if [ -z ${yn+x} ]; then
+    read -p "Do you wish to enter the container?(y/n)" yn
+fi
+
+while true; do
+    case $yn in
+        [Yy]* ) docker exec -it "${PWD##*/}_zf_1" bash; break;;
+        [Nn]* ) break;;
+        * ) read -p "Do you wish to enter the container?(y/n)" yn;;
+    esac
+done
