4d720c2bef9ba8e28d14dc7a8dc5901877b5a4aa
vijay <vijay@myvamla.com>
2021-03-04 16:07:28 +0530

    [REFACTOR] 19745 - Refactor victorFranchisedAutoDealer for excel mapping

--- clients/ArrowHead/view/apps/ArrowHead/Hidden Field/Documents/victor_FranchisedAutoDealer.js
+++ clients/ArrowHead/view/apps/ArrowHead/Hidden Field/Documents/victor_FranchisedAutoDealer.js
@@ -3,26 +3,59 @@ if (data.workbooksToBeGenerated.victor_FranchisedAutoDealer == true) {
   var checktotalLocationSalesRevenue = 0;
   var checklistCompaniesRepresented = [];
   var checkblanketcoverage = [];
+  var locationSpecificData = [];
 
-  data.locationSchedule.map(locationItem => {
-    if (locationItem.buildingDetails.length > 1) {
-      locationItem.buildingDetails.map(item => {
-        checkblanketcoverage.push({ result: "Premise Only" });
-      });
-    } else if (locationItem.buildingDetails.length == 1) {
-      checkblanketcoverage.push({ result: "No" });
+
+
+  if(data.locations.length == 1){
+    checkblanketcoverage.push({result:'No'});
+  }
+  else if(data.locations.length>1){
+    var currentLocation = 1;
+    var currentLocationBuildingCount = 0
+    var pushLocationsIntoArray = function(checkblanketcoverage,buildingCount){
+      var promise = new Promise(function(resolve,reject){
+        if(buildingCount>1){
+          checkblanketcoverage.push(...Array(currentLocationBuildingCount).fill({result:"Premise Only"}));
+          resolve();
+        }
+        else if(buildingCount == 1){
+          checkblanketcoverage.push({result:"No"});
+          resolve();
+        }
+        else{
+          resolve();
+        }
+        console.log(checkblanketcoverage);
+      })
+      return promise;
     }
-  });
+  
+    for(var i=0;i<data.locations.length;i++){
+      if(data.locations[i].locationNum==currentLocation){
+        currentLocationBuildingCount+=1
+        if(i==data.locations.length-1){
+          pushLocationsIntoArray(checkblanketcoverage,currentLocationBuildingCount);
+        }
+      }
+      else{
+        pushLocationsIntoArray(checkblanketcoverage,currentLocationBuildingCount);
+        currentLocationBuildingCount = 1;
+        currentLocation+=1;
+        if(i==data.locations.length-1){
+          pushLocationsIntoArray(checkblanketcoverage,currentLocationBuildingCount);
+        }
+      }
+    }
+  }
+
+  locationSpecificData = data.buildings.filter(building=>building.buildingNum == '1');
 
   checklistCompaniesRepresented = data.companiesRepresentedList.map(
     item => item.listcompaniesrepresented
   );
 
-  data.locationSchedule.map(item => {
-    checktotalLocationSalesRevenue += item.totalLocationSalesRevenue
-      ? item.totalLocationSalesRevenue
-      : 0;
-  });
+  checktotalLocationSalesRevenue += data.financialsYTDSalesTotal.total?data.financialsYTDSalesTotal.total:0;
 
   if (data.falsePretenseNumber <= 0) {
     falsePretenseLimit = "None";
@@ -47,17 +80,18 @@ if (data.workbooksToBeGenerated.victor_FranchisedAutoDealer == true) {
     checkcompaniesrepresented: checklistCompaniesRepresented + "",
     checktotalLocationSalesRevenue: checktotalLocationSalesRevenue,
     checkfalsePretenseLimit: falsePretenseLimit,
-    locationBuildingIndex: data.genericData.locationScheduleGridData
-      .map((i, index) => (index == 0 ? "invalid" : { result: i.locationNum }))
-      .filter(i => i !== "invalid"),
-    checkbuildingCoinsurance: data.genericData.locationScheduleGridData.map(
+    locationSpecificData: locationSpecificData,
+    locationBuildingIndex: data.locations
+    .map((i, index) => (index == 0 ? "invalid" : { result: i.locationNum }))
+    .filter(i => i !== "invalid"),
+    checkbuildingCoinsurance: data.locations.map(
       item => {
         return item.occupancyType == "vacantLand"
           ? { result: "" }
           : { result: "Agreed Amount" };
       }
     ),
-    checkpropertyCoverageDeductible: data.genericData.locationScheduleGridData.map(
+    checkpropertyCoverageDeductible: data.locations.map(
       item => {
         return item.occupancyType == "vacantLand"
           ? { result: "" }
@@ -68,10 +102,13 @@ if (data.workbooksToBeGenerated.victor_FranchisedAutoDealer == true) {
             };
       }
     ),
-    checkbuildingValuation: data.genericData.locationScheduleGridData.map(i => {
+
+    checkbuildingValuation: data.locations.map(i => {
       return { result: "Replacement Cost" };
     }),
-    CityStateZip: data.city + "," + data.state.name + "," + data.zip,
+
+    CityStateZip: data.city + "," + (data.state.name?data.state.name:data.state) + "," + data.zip,
+
     InsuranceContactNameandPhone: data.insurancecontactdataGrid
       ? data.insurancecontactdataGrid[0].genfirstname +
         " " +
@@ -79,61 +116,68 @@ if (data.workbooksToBeGenerated.victor_FranchisedAutoDealer == true) {
         "," +
         data.insurancecontactdataGrid[0].genphone
       : "",
-    checkSecurityGuards: data.genericData.locationScheduleGridData.some(
+
+    checkSecurityGuards: data.dol_Protection.some(
       locationItem => {
-        if (locationItem.dolsecguards == "yes") {
+        if (locationItem.premisesLotProtection.securityGuards == "yes") {
           return true;
         } else {
           return false;
         }
       }
     ),
-    checkdolaftrhrslighting: data.genericData.locationScheduleGridData.some(
+
+    checkdolaftrhrslighting: data.dol_Protection.some(
       locationItem => {
-        if (locationItem.dolaftrhrslighting == "yes") {
+        if (locationItem.premisesLotProtection.afterHoursLighting == "yes") {
           return true;
         } else {
           return false;
         }
       }
     ),
-    checkSurveillanceCamera: data.genericData.locationScheduleGridData.some(
+
+    checkSurveillanceCamera: data.dol_Protection.some(
       locationItem => {
-        if (locationItem.dolsurvcammoniintrunotifi == "yes") {
+        if (locationItem.entranceQuestions.camerasMonitored == "yes") {
           return true;
         } else {
           return false;
         }
       }
     ),
-    checkNotMonitoredSurveillanceCamera: data.genericData.locationScheduleGridData.some(
+
+    checkNotMonitoredSurveillanceCamera: data.dol_Protection.some(
       locationItem => {
-        if (locationItem.dolsurcamnotmoniotrd == "yes") {
+        if (locationItem.entranceQuestions.camerasNotMonitored == "yes") {
           return true;
         } else {
           return false;
         }
       }
     ),
-    checkdolpostnchain: data.genericData.locationScheduleGridData.some(
+
+    checkdolpostnchain: data.dol_Protection.some(
       locationItem => {
-        if (locationItem.dolEntranceQuestions.postChain == "yes") {
+        if (locationItem.entranceQuestions.postChain == "yes") {
           return true;
         } else {
           return false;
         }
       }
     ),
-    checkdolflyfencdpremises: data.genericData.locationScheduleGridData.some(
+
+    checkdolflyfencdpremises: data.dol_Protection.some(
       locationItem => {
-        if (locationItem.dolEntranceQuestions.fullyFencedPremises == "yes") {
+        if (locationItem.entranceQuestions.fullyFencedPremises == "yes") {
           return true;
         } else {
           return false;
         }
       }
     ),
-    checknumberofservicebays: data.genericData.locationScheduleGridData.some(
+
+    checknumberofservicebays: data.buildings.some(
       locationItem => {
         if (locationItem.numberofservicebays == "yes") {
           return true;
@@ -142,59 +186,10 @@ if (data.workbooksToBeGenerated.victor_FranchisedAutoDealer == true) {
         }
       }
     ),
-    garageLiabilityGrid: data.locationSchedule.map(locationItem => {
-      var oneItem = 0;
-      var twoItem = 0;
-      var threeItem = 0;
-      var fourItem = 0;
-      var fiveItem = 0;
-      var sixItem = 0;
-      var sevenItem = 0;
-      var eightItem = 0;
-      var nineItem = 0;
-      locationItem.buildingDetails.map(item => {
-        oneItem +=
-          item.fTEmployeesFurnishedAnAuto > 0
-            ? item.fTEmployeesFurnishedAnAuto
-            : 0;
-        twoItem +=
-          item.pTEmployeesFurnishedAnAuto > 0
-            ? item.pTEmployeesFurnishedAnAuto
-            : 0;
-        threeItem +=
-          item.fTEmployeesWhoAreNotFurnished > 0
-            ? item.fTEmployeesWhoAreNotFurnished
-            : 0;
-        fourItem +=
-          item.pTEmployeesWhoAreNotFurnished > 0
-            ? item.pTEmployeesWhoAreNotFurnished
-            : 0;
-        fiveItem += item.fTAllOtherEmployees > 0 ? item.fTAllOtherEmployees : 0;
-        sixItem += item.pTAllOtherEmployees > 0 ? item.pTAllOtherEmployees : 0;
-        sevenItem +=
-          item.nonEmployeesUnderTheAge > 0 ? item.nonEmployeesUnderTheAge : 0;
-        eightItem +=
-          item.nonEmployeesYearsOldorolder > 0
-            ? item.nonEmployeesYearsOldorolder
-            : 0;
-        nineItem +=
-          item.contractDriversNonEmployees > 0
-            ? item.contractDriversNonEmployees
-            : 0;
-      });
-      return [
-        oneItem,
-        twoItem,
-        threeItem,
-        fourItem,
-        fiveItem,
-        sixItem,
-        sevenItem,
-        eightItem,
-        nineItem
-      ];
-    }),
-    checkEmployeetoolsOnPremises: data.genericData.locationScheduleGridData.map(
+
+    
+
+    checkEmployeetoolsOnPremises: data.buildings.map(
       item => {
         return {
           result:
@@ -211,7 +206,8 @@ if (data.workbooksToBeGenerated.victor_FranchisedAutoDealer == true) {
         };
       }
     ),
-    checkTotalbuildinglimit: data.genericData.locationScheduleGridData.map(
+
+    checkTotalbuildinglimit: data.buildings.map(
       item => {
         return {
           result:
@@ -221,7 +217,8 @@ if (data.workbooksToBeGenerated.victor_FranchisedAutoDealer == true) {
         };
       }
     ),
-    checkOrdinanceAndUtility: data.genericData.locationScheduleGridData.map(
+ 
+    checkOrdinanceAndUtility: data.buildings.map(
       item => {
         return {
           result:
@@ -246,4 +243,74 @@ if (data.workbooksToBeGenerated.victor_FranchisedAutoDealer == true) {
     checkwhodeliversvehiclesonindependentContractor:
       data.whodeliversvehicleson == "independentContractor" ? "true" : "false"
   };
+
+  var getGarageLiabilityGrid = function(){
+
+    var currentLocation = 1;
+    var garageLiabilityGrid = [];
+  
+    var items = Array(9).fill(0);
+  
+    for(var i=0;i<data.employeeList.length;i++)
+    {
+      var locationNum = data.employeeList[i].locationBuildingNum.split("-")
+      locationNum = parseInt(locationNum[0])
+  
+      if(locationNum == currentLocation)
+      {
+        processBuildingEmployees(items,data.employeeList[i]);
+        if(i==data.employeeList.length-1){
+          garageLiabilityGrid.push(items);
+        }
+
+      }
+  
+      else{
+        currentLocation+=1;
+        garageLiabilityGrid.push(items);
+        items = Array(9).fill(0);
+        processBuildingEmployees(items,data.employeeList[i]);
+        if(i==data.employeeList.length-1){
+          garageLiabilityGrid.push(items);
+        }
+      }
+    }
+    return garageLiabilityGrid;
+  }
+  
+  var processBuildingEmployees = function(itemArray,employeeList){
+  
+      itemArray[0] +=
+        employeeList.total_fTEmployeesFurnishedAnAuto > 0
+          ? employeeList.total_fTEmployeesFurnishedAnAuto
+          : 0;
+       itemArray[1]+=
+        employeeList.total_pTEmployeesFurnishedAnAuto > 0
+          ? employeeList.total_pTEmployeesFurnishedAnAuto
+          : 0;
+      itemArray[2] +=
+        employeeList.total_fTEmployeesWhoAreNotFurnished > 0
+          ? employeeList.total_fTEmployeesWhoAreNotFurnished
+          : 0;
+      itemArray[3] +=
+        employeeList.total_pTEmployeesWhoAreNotFurnished > 0
+          ? employeeList.total_pTEmployeesWhoAreNotFurnished
+          : 0;
+      itemArray[4] += employeeList.total_fTAllOtherEmployees > 0 ? employeeList.total_fTAllOtherEmployees : 0;
+      itemArray[5] += employeeList.total_pTAllOtherEmployees > 0 ? employeeList.total_pTAllOtherEmployees : 0;
+      itemArray[6] +=
+        employeeList.total_nonEmployeesUnderTheAge > 0 ? employeeList.total_nonEmployeesUnderTheAge : 0;
+      itemArray[7] +=
+        employeeList.total_nonEmployeesYearsOldorolder > 0
+          ? employeeList.total_nonEmployeesYearsOldorolder
+          : 0;
+      itemArray[8] +=
+        employeeList.total_contractDriversNonEmployees > 0
+          ? employeeList.total_contractDriversNonEmployees
+          : 0;
+  }
+
+  value['garageLiabilityGrid']= getGarageLiabilityGrid();s
 }
+
+
