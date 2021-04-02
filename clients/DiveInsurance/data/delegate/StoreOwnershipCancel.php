dbcf2efce1d30c0c67fb350938b5a6857afad111
nehar <neha@myvamla.com>
2021-04-02 16:54:34 +0530

    [TASK]21130: Change ownership need not be sent to Insured at start

--- clients/DiveInsurance/data/delegate/StoreOwnershipCancel.php
+++ clients/DiveInsurance/data/delegate/StoreOwnershipCancel.php
@@ -17,21 +17,24 @@ class StoreOwnershipCancel extends AbstractAppDelegate
 
     public function execute(array $data, Persistence $persistenceService)
     {
-        if(isset($data['iterations'])){	
-			if($data['iterations'] != 0){
-				$cancelData = array();
-				$cancelData = is_string($data['old_data']) ? json_decode($data['old_data'],true) : $data['old_data'];
-				$cancelData['fileId'] = $data['assocId'];
-				$cancelData['cancellationStatus'] = 'approved';
-				if($data['iterations'] == 1){
-					$cancelData['transfer'] = false;
-				}
-				unset($data['old_data']);
-				$cancelData['reasonforCsrCancellation'] = '{"value": "Change Of Ownership"}';
-				$this->executeDelegate('CancelPolicy',$cancelData); 
-				$this->executeDelegate('DispatchCancelPolicyNotification',$cancelData); 
-			}
-		}
+        $data['changeOfOwnership'] = isset($data['changeOfOwnership']) ? $data['changeOfOwnership'] : "no";
+        if($data['changeOfOwnership'] == "yes"){
+            if(isset($data['iterations'])){	
+                if($data['iterations'] != 0){
+                    $cancelData = array();
+                    $cancelData = is_string($data['old_data']) ? json_decode($data['old_data'],true) : $data['old_data'];
+                    $cancelData['fileId'] = $data['assocId'];
+                    $cancelData['cancellationStatus'] = 'approved';
+                    if($data['iterations'] == 1){
+                        $cancelData['transfer'] = false;
+                    }
+                    unset($data['old_data']);
+                    $cancelData['reasonforCsrCancellation'] = '{"value": "Change Of Ownership"}';
+                    $this->executeDelegate('CancelPolicy',$cancelData); 
+                    $this->executeDelegate('DispatchCancelPolicyNotification',$cancelData); 
+                }
+            }
+        }
         return $data;
     }
 }
