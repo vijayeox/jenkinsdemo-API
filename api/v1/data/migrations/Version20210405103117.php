636348c0b444f65fd43e296b6f9a6d67b17792d6
bharatg <bharatg@myvamla.com>
2021-04-08 14:37:16 +0530

    [BUG] Fix Failing Migration

--- api/v1/data/migrations/Version20210405103117.php
+++ api/v1/data/migrations/Version20210405103117.php
@@ -20,22 +20,22 @@ final class Version20210405103117 extends AbstractMigration
 
   public function up(Schema $schema) : void
   {
-    $this->addSql("CREATE TABLE `oxzion_api`.`ox_user_announcement_mapper` (
+    $this->addSql("CREATE TABLE `ox_user_announcement_mapper` (
       `id` INT NOT NULL AUTO_INCREMENT,
       `user_id` INT NOT NULL,
       `announcement_id` INT NOT NULL,
       `view` TINYINT NOT NULL DEFAULT 0,
       PRIMARY KEY (`id`),
-      INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
-      INDEX `announcement_id_idx` (`announcement_id` ASC) VISIBLE,
+      INDEX `user_id_idx` (`user_id` ASC),
+      INDEX `announcement_id_idx` (`announcement_id` ASC),
       CONSTRAINT `user_id`
       FOREIGN KEY (`user_id`)
-      REFERENCES `oxzion_api`.`ox_user` (`id`)
+      REFERENCES `ox_user` (`id`)
       ON DELETE CASCADE
       ON UPDATE NO ACTION,
       CONSTRAINT `announcement_id`
       FOREIGN KEY (`announcement_id`)
-      REFERENCES `oxzion_api`.`ox_announcement` (`id`)
+      REFERENCES `ox_announcement` (`id`)
       ON DELETE CASCADE
       ON UPDATE NO ACTION)
       ENGINE = InnoDB
