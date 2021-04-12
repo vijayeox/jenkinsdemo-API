b14566db17e9ede55510516e40e73d4c70ad40be
bharatg <bharatg@myvamla.com>
2021-04-12 18:25:05 +0530

    [BUG] KRA Admin Configuration

--- api/v1/lib/Oxzion/src/Model/Kra.php
+++ api/v1/lib/Oxzion/src/Model/Kra.php
@@ -16,6 +16,7 @@ class Kra extends Entity
         'type' =>                   ['type' => Type::STRING,    'readonly' => false, 'required' => false],
         'name' =>                   ['type' => Type::STRING,    'readonly' => false, 'required' => false],
         'business_role_id' =>       ['type' => Type::INTEGER,   'readonly' => false, 'required' => false],
+        'status' =>                 ['type' => Type::STRING,    'readonly' => false, 'required' => false],
         'user_id' =>                ['type' => Type::INTEGER,   'readonly' => false, 'required' => false],
         'team_id' =>                ['type' => Type::INTEGER,   'readonly' => false, 'required' => false],
         'date_created' =>           ['type' => Type::TIMESTAMP, 'readonly' => true,  'required' => false],
