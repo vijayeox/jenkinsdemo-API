57923699699d863974c6128257cc434b18a7daf6
brian <brianmp@eoxvantage.in>
2021-04-14 17:00:46 +0530

    [BUG]21522 - Producer code to pull directly from name

--- clients/ArrowHead/data/delegate/UserMigration.php
+++ clients/ArrowHead/data/delegate/UserMigration.php
@@ -17,7 +17,7 @@ class UserMigration extends AbstractDocumentAppDelegate
         $this->logger->info("User Migration Delegate ---".print_r($data,true));
         try {
             $role = implode(",", $data['role']);
-            $insertQuery = "INSERT INTO user (`uuid`,`username`,`firstname`,`lastname`,`email`,`role`,`producer_code`) VALUES ('".$data['uuid']."','".$data['username']."','".$data['firstname']."','".$data['lastname']."','".$data['email']."','".$role."', NULL);";
+            $insertQuery = "INSERT INTO user (`uuid`,`username`,`firstname`,`lastname`,`email`,`role`,`producer_code`,`name`) VALUES ('".$data['uuid']."','".$data['username']."','".$data['firstname']."','".$data['lastname']."','".$data['email']."','".$role."', NULL,'".$data['name']."');";
             $persistenceService->insertQuery($insertQuery);
             return true;
         } catch (Exception $e) {
