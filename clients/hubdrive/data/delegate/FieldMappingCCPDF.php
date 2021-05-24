0fdc6c5270dfc00f9841e96fd43e7994e34781f6
Supreetha D <supreethad@eoxvantage.in>
2021-05-24 11:38:20 +0000

    [REQ]#22706 Field Mapping delegate for PDF mapping

---
+++ clients/hubdrive/data/delegate/FieldMappingCCPDF.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    "CC" => [
+        "text" =>
+        [
+            'companyName' => 'companyName',
+            'personalInfoFirstName' => 'personalInfoFirstName',
+            'personalInfoLastName' => 'personalInfoLastName',
+            'personalInfoDOT' => 'personalInfoDOT',
+        ],
+    ]
+);
+
+
+
+
+
