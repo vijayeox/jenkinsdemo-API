6b6164ebbe04e81fbaadf794fd40e887c860b259
Supreetha D <supreethad@eoxvantage.in>
2021-05-28 14:19:22 +0000

    changes for key values for pdf mapping,email delagte and esign

--- clients/hubdrive/data/delegate/CCPDFGeneration.php
+++ clients/hubdrive/data/delegate/CCPDFGeneration.php
@@ -70,7 +70,7 @@ class CCPDFGeneration extends AbstractDocumentAppDelegate
         $signers = array(
                 "name"=>$selectedTemplate,
                 "message"=>"Please sign",
-                "signers"=>[['participant' => ["email"=>$data['personalInfoEmail'], 'name' => $data['personalInfoFirstName']],
+                "signers"=>[['participant' => ["email"=>$data['email'], 'name' => $data['firstname']],
                             "fields"=> $field]]);
         $docId = $this->setupDocument($fileUUID."_".$selectedTemplate,$documentDestination,$signers);
         $signingLink = $this->getDocumentSigningLink($docId);
