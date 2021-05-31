88ee34ed8914762ff86c2fc6ca8ae4228c31433e
brian <brianmp@eoxvantage.in>
2021-05-31 18:36:00 +0530

    [REFACTOR] - Changed protocol to suite new elastic server

--- integrations/camel/src/main/groovy/com/oxzion/routes/ElasticClientIndexer.groovy
+++ integrations/camel/src/main/groovy/com/oxzion/routes/ElasticClientIndexer.groovy
@@ -52,7 +52,7 @@ class ElasticClientIndexer extends RouteBuilder {
                         def object = jsonSlurper.parseText(exchange.getMessage().getBody())
                         def HOST = env.getProperty("elastic.host")
                         def CORE = env.getProperty("elastic.core")
-                        def USERNAME = env.getProperty("elastic.user")
+                        def USERNAME = env.getProperty("elastic.username")
                         def PASSWORD = env.getProperty("elastic.password")
                         int PORT = env.getProperty("elastic.port").toInteger()
                         def idList,deleteList
@@ -76,7 +76,7 @@ class ElasticClientIndexer extends RouteBuilder {
                         new BasicCredentialsProvider();
                         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(USERNAME, PASSWORD));
                         def output = JsonOutput.toJson(object.body)
-                        RestClientBuilder builder = RestClient.builder(new HttpHost(HOST, PORT, "http")).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                        RestClientBuilder builder = RestClient.builder(new HttpHost(HOST, PORT, "https")).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
                             @Override
                             public HttpAsyncClientBuilder customizeHttpClient(
                                 HttpAsyncClientBuilder httpClientBuilder) {
