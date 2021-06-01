40d27d10bc3d15bcddcc9fdc3b807e0b0d5fab1c
supreethad <supreethad@eoxvantage.in>
2021-06-01 10:47:52 +0530

    Fixes for pdf mapping key in CC document

--- clients/hubdrive/data/delegate/FieldMappingCCPDF.php
+++ clients/hubdrive/data/delegate/FieldMappingCCPDF.php
@@ -4,7 +4,7 @@ return array(
         "text" =>
         [
             'companyName' => 'companyName',
-            'firstname' => 'personalInfoFirsttName',
+            'firstname' => 'personalInfoFirstName',
             'lastname' => 'personalInfoLastName',
             'personalInfoDOT' => 'personalInfoDOT',
             'mc' => 'untitled3'
