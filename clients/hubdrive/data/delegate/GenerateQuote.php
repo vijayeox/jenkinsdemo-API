8cb316de8be1ce1e05167289d5e3206df91349ce
nehar <neha@myvamla.com>
2021-09-28 18:57:34 +0530

    [TASK 26103]: Quote generation

---
+++ clients/hubdrive/data/delegate/GenerateQuote.php
@@ -0,0 +1,62 @@
+<?php
+
+use Oxzion\AppDelegate\AbstractDocumentAppDelegate;
+use Oxzion\Db\Persistence\Persistence;
+use Oxzion\Utils\ArtifactUtils;
+use Oxzion\AppDelegate\FileTrait;
+use Oxzion\Auth\AuthContext;
+use Oxzion\Auth\AuthConstants;
+use Oxzion\AppDelegate\AccountTrait;
+use Oxzion\Utils\FileUtils;
+
+class GenerateQuote extends AbstractDocumentAppDelegate
+{
+    use FileTrait;
+    use AccountTrait;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function execute(array $data, Persistence $persistenceService)
+    {
+        $template = "ExcessQuote";
+        $quotePdfName = 'Quote_'.$data['insuredName'].".pdf";
+        $generatedQuotePDF = array();
+        $fileUUID = isset($data['uuid']) ? $data['uuid'] : $data['fileId'];
+        $currentAccount = isset($data['accountId']) ? $data['accountId'] : null;
+        $accountId = isset($data['accountName']) ? $this->getAccountByName($data['accountName']) : (isset($currentAccount) ? $currentAccount : AuthContext::get(AuthConstants::ACCOUNT_UUID));
+        $fileData = $this->getFile($data['fileId'],false,$data['accountId']);
+        $data = array_merge($data,$fileData['data']);
+        $this->logger->info("ACCOUT IS ____" . $accountId);
+        $path = "https://i.imgur.com/1zkaS1p.jpeg";
+        $img = file_get_contents($path);
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data['avantImageSrc'] = 'data:image/' . $type . ';base64,' . base64_encode($img);
+        $folderDestination =  ArtifactUtils::getDocumentFilePath($this->destination, $fileUUID, array('accountId' => $accountId));
+        $fileDestination = $folderDestination['absolutePath'].$quotePdfName;
+        if(FileUtils::fileExists($fileDestination)) {
+            FileUtils::deleteFile($quotePdfName,$folderDestination['absolutePath']);
+        }
+        $doc = $this->documentBuilder->generateDocument($template,$data,$fileDestination);
+        $documentpdf = $folderDestination['relativePath'] . $quotePdfName;
+        if (isset($data['attachments'])) {
+            $data['attachments'] = json_decode($data['attachments'], true);
+            array_push($data['attachments'],array(
+                "name" => $quotePdfName,
+                "fullPath" => $fileDestination,
+                "file" => $documentpdf,
+                "originalName" => $quotePdfName,
+                "type" => "file/pdf",
+            ));
+        }else{
+            $data['attachments'] = $generatedQuotePDF;
+        }
+        if(isset($data['avantImageSrc'])){
+            unset($data['avantImageSrc']);
+        }
+        $this->saveFile($data, $fileUUID);
+        return $data;
+    }
+}
