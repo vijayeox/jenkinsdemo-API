7172fdf8eba4a4f18e5c1e91d4ec475655fb120b
nehar <neha@myvamla.com>
2021-09-28 09:36:03 +0530

    [TASK 26098]: Avant submission to Genre

--- clients/hubdrive/data/delegate/ExcessMail.php
+++ clients/hubdrive/data/delegate/ExcessMail.php
@@ -2,10 +2,10 @@
 
 use Oxzion\AppDelegate\MailDelegate;
 use Oxzion\Db\Persistence\Persistence;
-
+use Oxzion\AppDelegate\FileTrait;
 class ExcessMail extends MailDelegate
 {
-
+    use FileTrait;
     public function setDocumentPath($destination)
     {
         $this->destination = $destination;
@@ -14,13 +14,15 @@ class ExcessMail extends MailDelegate
     public function execute(array $data, Persistence $persistenceService)
     {
         $this->logger->info("Executing Excess Mail with data- " . print_r($data, true));
+        $fileData = $this->getFile($data['fileId'],false,$data['accountId']);
+        $data = array_merge($data,$fileData['data']);
         // Add logs for created by id and producer name who triggered submission
         $selectQuery = "Select value FROM applicationConfig WHERE type ='excessLiabilityMail'";
         $mailTo = ($persistenceService->selectQuery($selectQuery))->current()["value"];
         $mailOptions = [];
         $mailOptions['to'] = $mailTo;
         $fileData = array();
-        $mailOptions['subject'] = 'HUB Drive Excess Liability Shield';
+        $mailOptions['subject'] = 'HUB Drive Excess Liability Document Submission -'.$data['SubmissionNumber'];
         $template = 'excessLiabilityMail';
         if (isset($data['attachments'])) {
             $data['attachments'] = json_decode($data['attachments'], true);
---
+++ clients/hubdrive/data/template/confirmationMail.tpl
@@ -0,0 +1,17 @@
+{* ConfirmationMail *}
+
+            <div style='width:100%;background:#452767;color:#fff;height:35px;margin-bottom:2px'>
+            </div>
+            <div style='line-height: 24px'>Dear {$producersName}, <br/>
+                <p>
+                    Thank you for your submission.   We at Avant Brokerage appreciate your business.   Please access this email as notice that we have received your submission and will begin the review process.   If anything is missing in order for us to provide a quotation, we will be in contact within the next 24 hours.After we have a completed submission, we will work to provide your quotation within the next 3 business days.
+                <p>
+                <p>
+                    Thank you again for your business and we will be in contact soon.
+                </p>
+			 <br/>
+
+            </div>
+            
+The Avant Brokerage Team
+<br/><br/>
