a333c7d21dbf1bf2d19b28449f25cd16a357e8f7
nehar <neha@myvamla.com>
2021-10-28 16:18:37 +0530

    SSO Integration

---
+++ clients/hubdrive/data/delegate/InsureLearnSso.php
@@ -0,0 +1,18 @@
+<?php
+
+use Oxzion\AppDelegate\InsuranceTrait;
+
+class InsureLearnSso
+{
+    use InsuranceTrait;
+
+    public function execute(array $data)
+    {
+        $this->setInsuranceConfig([
+            "client" => "InsureLearn"
+        ]);
+
+        return ['SSOLink' => $this->insuranceService->getSsoLink($data['email'], 'Welcome2eox!')];
+    }
+
+}
