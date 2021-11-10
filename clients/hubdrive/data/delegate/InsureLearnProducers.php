126891728718a57149f4e8c4199c080ee863b099
meghag <meghag@eoxvantage.in>
2021-11-10 12:41:56 +0530

    REQ 27996 - Adding producers to InsureLearn

--- clients/hubdrive/data/delegate/InsureLearnProducers.php
+++ clients/hubdrive/data/delegate/InsureLearnProducers.php
@@ -22,12 +22,13 @@ class InsureLearnProducers extends AbstractAppDelegate
         // if (!empty($data['debug'])) $data = $this->getSampleData();
         $this->logger->info("HERE INSURE LEARN");
         //if ($data['insureLearnIntegration'] == 'no') return $data;
-
+        
+        $data['companyName'] = 'Hub International';
         $contractorGroup = $this->insuranceService->search('groupName', [
             'groupName' => $data['companyName']
         ]);
         
-        if (empty($contractorGroup['group'])) {
+        /*if (empty($contractorGroup['group'])) {
             $this->insuranceService->create('group', [
                 'groupName' => $data['companyName'],
                 'groupTypeID' => 2,
@@ -37,115 +38,64 @@ class InsureLearnProducers extends AbstractAppDelegate
             $contractorGroup = $this->insuranceService->search('groupName', [
                 'groupName' => $data['companyName']
             ]);
-        }
+        }*/
         $this->logger->info("Contractor Group". print_r($contractorGroup, true));
         //$this->checkManager($data, $contractorGroup['group']);
-        $this->checkDriver($data, $contractorGroup['group']);
-        $this->logger->info("HERE DRIVER INSURE LEARN ADDEDD");
+        $this->checkProducer($data, $contractorGroup['group']);
+        $this->logger->info("HERE data INSURE LEARN ADDEDD");
         return $data;
     }
 
-    private function checkManager($data, $group)
+    
+
+    private function checkProducer($data, $group)
     {
         $data['email'] = strtolower($data['email']);
-        $contractorUser = $this->insuranceService->search('userData', [
+        $dataUser = $this->insuranceService->search('userData', [
             'loginID' => $data['email']
         ]);
-        $contractorUserData = [
+        $dataUserData = [
             'loginID' => $data['email'],
             'userData' => $data['email'],
             'passwd' => 'Welcome2eox!',
-            'firstName' => $data['firstname'],
-            'lastName' => $data['lastname'],
-            'addr1' => $data['address1'],
-            'city' => $data['city'],
-            'state' => $data['stateObj']['abbreviation'],
-            'zip' => $data['zip'],
-            'email' => $data['email'],
-            'phone' => $data['phone']
+            'firstName' => $data['firstName'],
+            'middleName' => $data['middleName'],
+            'lastName' => $data['lastName'],
+            'email' => $data['email']
         ];
-        if (empty($contractorUser['userProfiles'])) {
-            $this->insuranceService->create('user', $contractorUserData + [
-                'accessLevel' => 2,
-                'accessMode' => 5400,
-                'roleName' => 'Safety Director/Manager/Owner',
+        if (empty($dataUser['userProfiles'])) {
+            $this->insuranceService->create('user', $dataUserData + [
+                'accessLevel' => 1,
+                'accessMode' => 0,
+                'roleName' => 'IC Driver - English',
                 'locationName' => $group['@attributes']['groupName']
             ]);
-            $contractorUser = $this->insuranceService->search('userData', [
+            $dataUser = $this->insuranceService->search('userData', [
                 'loginID' => $data['email']
             ]);
-            $contractorUser = $contractorUser['userProfiles']['userProfile'];
         } else {
-            $contractorUser = $contractorUser['userProfiles']['userProfile'];
-            $this->insuranceService->create('user', $contractorUserData);
+            $this->insuranceService->create('user', $dataUserData);
+            $dataUser = $dataUser['userProfiles']['userProfile'];
             $this->insuranceService->perform('group', [
                 'groupID' => $group['@attributes']['groupID'],
-                'groupTypeID' => 2,
-                'userProfileID' => $contractorUser['@attributes']['userProfileID']
-            ], 'adduser');
-            $this->insuranceService->perform('group', [
-                'groupID' => 8,
                 'groupTypeID' => 1,
-                'userProfileID' => $contractorUser['@attributes']['userProfileID']
+                'userProfileID' => $dataUser['@attributes']['userProfileID']
             ], 'adduser');
+            $groupdatas[] = $dataUser['@attributes']['userProfileID'];
         }
-
-        $this->insuranceService->perform('group', [
-            'groupID' => $group['@attributes']['groupID'],
-            'groupTypeID' => 2,
-            'userProfileID' => $contractorUser['@attributes']['userProfileID']
-        ], 'addmanager');
-    }
-
-    private function checkDriver($data, $group)
-    {
-        foreach ($data['userGrid'] as $driver) {
-            $driver['email'] = strtolower($driver['email']);
-            $driverUser = $this->insuranceService->search('userData', [
-                'loginID' => $driver['email']
-            ]);
-            $driverUserData = [
-                'loginID' => $driver['email'],
-                'userData' => $driver['email'],
-                'passwd' => 'Welcome2eox!',
-                'firstName' => $driver['firstName'],
-                'middleName' => $driver['middleName'],
-                'lastName' => $driver['lastName'],
-                'email' => $driver['email']
-            ];
-            if (empty($driverUser['userProfiles'])) {
-                $this->insuranceService->create('user', $driverUserData + [
-                    'accessLevel' => 1,
-                    'accessMode' => 0,
-                    'roleName' => 'Hub Producer',
-                    'locationName' => $group['@attributes']['groupName']
-                ]);
-                $driverUser = $this->insuranceService->search('userData', [
-                    'loginID' => $driver['email']
-                ]);
-            } else {
-                $this->insuranceService->create('user', $driverUserData);
-                $driverUser = $driverUser['userProfiles']['userProfile'];
-                $this->insuranceService->perform('group', [
-                    'groupID' => $group['@attributes']['groupID'],
-                    'groupTypeID' => 1,
-                    'userProfileID' => $driverUser['@attributes']['userProfileID']
-                ], 'adduser');
-                $groupDrivers[] = $driverUser['@attributes']['userProfileID'];
-            }
-        }
-        if (!empty($groupDrivers)) {
+        
+        if (!empty($groupdatas)) {
             $this->insuranceService->perform('group', [
                 'groupID' => $group['@attributes']['groupID'],
                 'groupTypeID' => 2,
-                'userProfileID' => implode('|', $groupDrivers)
+                'userProfileID' => implode('|', $groupdatas)
             ], 'adduser');
         }
     }
 
     private function getSampleData()
     {
-        return json_decode('{
+        /*return json_decode('{
             "appId": "a4b1f073-fc20-477f-a804-1aa206938c42",
             "city": "dfgdfg",
             "companyName": "HUB EOX",
@@ -160,6 +110,15 @@ class InsureLearnProducers extends AbstractAppDelegate
             "firstname": "Meghana",
             "lastname": "Patil",
             "name": "Meghana Patil"
+        }', true);*/
+
+        return json_decode('{
+            "email": "supreetha@eoxvantage.com",
+            "firstName": "Supreetha",
+            "middleName" : "",
+            "lastName": "D"
+            
+            
         }', true);
     }
 
